3661aa1a4d6da05f3b34c1904a4134aaf68e0199
piberger <pberger@phys.ethz.ch>
2017-03-08 21:12:00 +0100

    add more verbose output to verify merge script

--- python/verify_merge.py
+++ python/verify_merge.py
@@ -10,6 +10,8 @@ if len(sys.argv) < 2:
 basePath = sys.argv[1]
 globPath = basePath + "/*.root"
 rootFileNames = glob.glob(globPath)
+totalFiles = 0
+allFilesSize = 0
 for rootFileName in rootFileNames:
     size = os.stat(rootFileName).st_size
     sampleName = rootFileName.split('/')[-1].replace('ZmmH.BestCSV.heppy.','').replace('.root','')
@@ -19,10 +21,14 @@ for rootFileName in rootFileNames:
     rootFileNamesSplit = glob.glob(globPathSplit)
     nFiles = 0
     totalSize = 0
+    fileSizes = []
     for rootFileNameSplit in rootFileNamesSplit:
         nFiles += 1
-        totalSize += os.stat(rootFileNameSplit).st_size
-
+        fileSize = os.stat(rootFileNameSplit).st_size
+        totalSize += fileSize
+        fileSizes.append(fileSize)
+        allFilesSize += fileSize
+    totalFiles += nFiles
     difference = 1.0 * size / totalSize
     if difference < 0:
         difference = -difference
@@ -31,6 +37,10 @@ for rootFileName in rootFileNames:
         print " => \x1b[31mWARNING: merged: {mergedSize} all files: {totalSize} fraction: {diff} percent\x1b[0m".format(mergedSize=size, totalSize=totalSize, diff=100.0*difference)
     else:
         print " => \x1b[32mmerged: {mergedSize} all files: {totalSize}\x1b[0m".format(mergedSize=size, totalSize=totalSize)
+    print " => mean file size (in kB) %d, min: %d, max: %d:"%(sum(fileSizes)/len(fileSizes)/1024.0 if len(fileSizes) > 0 else 0,min(fileSizes)/1024.0, max(fileSizes)/1024.0)    
+    print " => n files:", nFiles
 
-
+print "total files:", totalFiles
+print "merged files:", len(rootFileNames)
+print "total size:", allFilesSize
 
