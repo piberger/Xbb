bdd1817f10f61dce6327c04c7ba779a6abe30edf
piberger <pberger@phys.ethz.ch>
2017-09-14 13:16:16 +0200

    add test class for split tree cache

---
+++ python/test_treecache2.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import math
+import ROOT
+ROOT.gROOT.SetBatch(True)
+
+from myutils import NewTreeCache as TreeCache
+from myutils.sampleTree import SampleTree as SampleTree
+
+numSampleFiles = SampleTree.countSampleFiles('test/bkg.txt')
+splitFiles = 7
+numParts = int(math.ceil(float(numSampleFiles) / splitFiles))
+print ("found: %d files"%numSampleFiles)
+
+#
+numEventsPassed = 0
+
+# cache all parts sequentially for this test, of course one can do this in parallel...
+for i in range(1, numParts+1):
+    print ("cache part %d of %d"%(i, numParts))
+
+    sampleTreeBkg = SampleTree('test/bkg.txt', splitFiles=splitFiles, splitFilesPart=i)
+    tc = TreeCache.TreeCache(
+        sample='ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8_ext1',
+        cutList='V_pt>100',
+        inputFolder='/scratch/p/',
+        tmpFolder='/scratch/p/tmp/',
+        outputFolder='/scratch/p/cache/',
+        cachePart=i,
+        cacheParts=numParts,
+        splitFiles=splitFiles,
+        debug=True
+    )
+
+    # run caching
+    tc.setSampleTree(sampleTreeBkg).cache()
+    sampleTreeBkg.process()
+    tc.moveFilesToFinalLocation()
+
+    numEventsPassed += sampleTreeBkg.outputTrees[0]['passed']
+
+# test again!
+tc = TreeCache.TreeCache(
+    sample='ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8_ext1',
+    cutList='V_pt>100',
+    inputFolder='/scratch/p/',
+    tmpFolder='/scratch/p/tmp/',
+    outputFolder='/scratch/p/cache/',
+    splitFiles=splitFiles,
+    cacheParts=numParts,
+    debug=True
+)
+isCachedAndValid = tc.isCachedAndValid()
+print ('is cached and valid? ', isCachedAndValid)
+
+if isCachedAndValid:
+    cachedSampleTree = tc.getTree()
+    print ("CACHE:    ", cachedSampleTree.tree.GetEntries())
+    print ("EXPECTED: ", numEventsPassed)
+    if (cachedSampleTree.tree.GetEntries() == numEventsPassed):
+        print ('OK!')
+    else:
+        print ('MISMATCH!')
+else:
+   raise Exception("CACHE ERROR!!!")
+
