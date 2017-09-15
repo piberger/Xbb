563f22a120b3fccf281b6a237108be3c49e18201
piberger <pberger@phys.ethz.ch>
2017-09-15 14:45:38 +0200

    update mva caching test script

--- python/test_cache_training.py
+++ python/test_cache_training.py
@@ -4,68 +4,78 @@ ROOT.gROOT.SetBatch(True)
 
 from myutils import NewTreeCache as TreeCache
 from myutils.sampleTree import SampleTree as SampleTree
-from myutils import BetterConfigParser, mvainfo, ParseInfo
+from myutils import BetterConfigParser, ParseInfo
 
 import os
 
-user = os.popen('whoami').read().strip('\n').strip('\r')
+# ----------------------------------------------------------------------------------------------------------------------
+# configuration
+# ----------------------------------------------------------------------------------------------------------------------
 testConfiguration = 'TestZll2016config/'
+backgroundSampleNames = ['HT100to200ZJets_udscg_ext1', 'HT100to200ZJets_1b_ext1', 'HT100to200ZJets_2b_ext1']
+signalSampleNames = ['ZH_HToBB_ZToLL_M125_pow_ext1']
+samplesToCache = [
+    'DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1',
+    'ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8_ext1',
+]
+TrainCut = '!((evt%2)==0||isData)'
+EvalCut = '((evt%2)==0||isData)'
 
 if os.path.exists("../interface/DrawFunctions_C.so"):
     print 'ROOT.gROOT.LoadMacro("../interface/DrawFunctions_C.so")'
     ROOT.gROOT.LoadMacro("../interface/DrawFunctions_C.so")
 
-#load config
+# ----------------------------------------------------------------------------------------------------------------------
+# load config
+# ----------------------------------------------------------------------------------------------------------------------
 config = BetterConfigParser()
 config.read(testConfiguration + '/paths.ini')
 config.read(testConfiguration + '/general.ini')
 config.read(testConfiguration + '/samples_nosplit.ini')
 
-samplesPath = '/scratch/{user}/'.format(user=user)
+samplesPath = config.get('Directories', 'MVAin')
 samplesinfo = testConfiguration + '/samples_nosplit.ini'
 info = ParseInfo(samplesinfo, samplesPath)
-print info
 
-#sampleToCache = 'DYJetsToLL_Pt-100To250_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8'
-sampleToCache = 'DYJetsToLL_M-50_HT-100to200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1'
-#sampleToCache = 'ZH_HToBB_ZToLL_M125_13TeV_powheg_pythia8_ext1'
+cachedPath = config.get('Directories', 'tmpSamples')
+tmpPath = config.get('Directories', 'scratch')
 
 sampleFilesFolder = config.get('Directories', 'samplefiles')
-samples = info.get_samples(['HT100to200ZJets_udscg_ext1', 'HT100to200ZJets_1b_ext1', 'HT100to200ZJets_2b_ext1', 'ZH_HToBB_ZToLL_M125_pow_ext1'])
-sampleFilesListFileName = sampleFilesFolder + sampleToCache + '.txt'
 
+samples = info.get_samples(backgroundSampleNames + signalSampleNames)
 
-sampleTree = SampleTree(sampleFilesListFileName)
+# ----------------------------------------------------------------------------------------------------------------------
+# cache samples
+# ----------------------------------------------------------------------------------------------------------------------
+# for this test run caching for all samples sequentially
+for sampleToCache in samplesToCache:
 
-TrainCut = '!((evt%2)==0||isData)'
-EvalCut = '((evt%2)==0||isData)'
+    sampleFilesListFileName = sampleFilesFolder + sampleToCache + '.txt'
+    sampleTree = SampleTree(sampleFilesListFileName)
 
-# prepare caches for training and evaluation samples
-treeCaches = []
-for sample in samples:
-    print sample.name, "\x1b[31m", sample.subcut, "\x1b[0m"
-    print " =>", sample.identifier
+    # prepare caches for training and evaluation samples
+    treeCaches = []
+    for sample in samples:
 
-    if sample.identifier == sampleToCache:
-        for additionalCut in [TrainCut, EvalCut, None]:
-            treeCaches.append(
-                TreeCache.TreeCache(
-                    sample=sample.name,
-                    cutList=('(%s)&&(%s)'%(sample.subcut, additionalCut)) if additionalCut else sample.subcut,
-                    inputFolder='/scratch/' + user + '/',
-                    tmpFolder='/scratch/' + user + '/tmp/',
-                    outputFolder='/scratch/' + user + '/cache/',
-                    debug=True
-                ).setSampleTree(sampleTree).cache()
-            )
+        if sample.identifier == sampleToCache:
+            print sample.name, "\x1b[31m", sample.subcut, "\x1b[0m"
+            print " =>", sample.identifier
 
-if len(treeCaches) > 0:
-    # run on the tree
-    sampleTree.process()
+            for additionalCut in [TrainCut, EvalCut, None]:
+                treeCaches.append(
+                    TreeCache.TreeCache(
+                        sample=sample.name,
+                        cutList=('(%s)&&(%s)'%(sample.subcut, additionalCut)) if additionalCut else sample.subcut,
+                        inputFolder=samplesPath,
+                        tmpFolder=tmpPath,
+                        outputFolder=cachedPath,
+                        debug=True
+                    ).setSampleTree(sampleTree).cache()
+                )
 
-    # move files to final location
-    for cache in treeCaches:
-        cache.moveFilesToFinalLocation()
-else:
-    print "noting to do!"
+    if len(treeCaches) > 0:
+        # run on the tree
+        sampleTree.process()
 
+    else:
+        print "noting to do!"
