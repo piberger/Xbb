7e8fcc1f086d92ef555c0a0584706213499a8a4e
Gael Perrin <gael.ludovic.perrin@cern.ch>
2018-02-01 19:11:17 +0100

    Improved macro for sample comparison

--- python/ZllSync/CompareSamples.py
+++ python/ZllSync/CompareSamples.py
@@ -1,5 +1,7 @@
 import ROOT
+import os
 import sys
+import subprocess
 from copy import copy
 
 ROOT.gROOT.SetBatch(True)
@@ -8,23 +10,14 @@ ROOT.gSystem.Load("../../interface/VHbbNameSpace_h.so")
 
 path_prefix = '/mnt/t3nfs01/data01/shome/gaperrin/VHbb/CMSSW_7_4_3/src/Xbb/python/WlvSync'
 
-#RegionList = ['TTel','ZLFel','ZHFel','TTul','ZLFul','ZHFul','TTeh','ZLFeh','ZHFeh','TTuh','ZLFuh','ZHFuh']
-RegionList = ['TTel']
-SampleList = ['TT','ZHbb','DY400']
-#total
-#VarList = ['PU','genSign','bTag','dEtabb', 'NLOVpt', 'NLOewk','Wpt','eSelSF','uSelSF','eTrigSF','uTrigSF']
-#VarList = ['PU','genSign','bTag','FitCorr', 'Vweight','LepSelSF','eTrigSF','uTrigSF']
-#VarList = ['PU', Vtype]
-#Delete those guys afterwards
-##Heiner
-#VarList = ['Vtype','lepPt','HT', 'CMVAmin',' Vmass', 'CMVAmax', 'Vpt','HVdPhi','lepRelIso1','nAddLeptons','lepMetDPhi','sigma_met_pt','jetleadpt','jetsubleadpt','puWeight','MET','BDT','Hmass','lepRelIso2','nAddJets']
-#SampleVar_AT = {'Vtype':'Vtype_new','lepPt':'vLeptons_new_pt','HT':'cutFlow','CMVAmin':'CMVAmin','Vmass':'Vmass','CMVAmax':'CMVAmax','Vpt':'Vpt','cutFlow':'cutFlow','HVdPhi':'HVdPhi','lepRelIso1':'lepRelIso1','nAddLeptons':'nAddLeptons','lepMetDPhi':'lepMetDPhi','sigma_met_pt':'sigma_met_pt','jetleadpt':'jetleadpt','jetsubleadpt':'jetsubleadpt','':'controlSample','PU':'puWeight','MET':'met_pt','MetTkMetDPhi':'MetTkMetDPhi','count':'count','BDT':'BDT','Hmass':'Hmass','lepRelIso2':'lepRelIso2','nAddJets':'nAddJets','minMetjDPhi':'minMetjDPhi')
 
-#Not sure (implementation)
-#
-#
-#
-VarList = ['Vtype']
+###########
+##General info (sample, region, variables)
+###########
+
+#All
+RegionList = ['TTel','ZLFel','ZHFel','TTul','ZLFul','ZHFul','TTeh','ZLFeh','ZHFeh','TTuh','ZLFuh','ZHFuh']
+SampleList = ['TT','ZHbb','DY400']
 #VarList = ['Vtype',
 #    'lepPt',
 #    'HT',
@@ -43,17 +36,24 @@ VarList = ['Vtype']
 #    'lepRelIso2'
 #]
 
-SampleVar_Xbb = {'Vtype':'Vtype_new','lepPt':'vLeptons_new_pt[0]','HT':'lheHT','CMVAmin':'Jet_btagCMVAV2[hJCMVAV2idx[1]]','Vmass':'V_mass','CMVAmax':'Jet_btagCMVAV2[hJCMVAV2idx[0]]','Vpt':'V_pt','HVdPhi':'VHbb::deltaPhi(HCMVA_phi,V_phi)','lepRelIso1':'vLeptons_new_relIso04[0]','jetleadpt':'Jet_pt[hJCMVAV2idx[0]]','jetsubleadpt':'Jet_pt[hJCMVAV2idx[1]]','PU':'puWeight','MET':'met_pt','count':'1','Hmass':'HCMVAV2_mass','lepRelIso2':'vLeptons_new_relIso04[0]','nAddJets':'Sum$(Jet_pt>30 && abs(Jet_eta)<5.2 && Jet_puId>=4)'}
+##Test
+#RegionList = ['TTel']
+#SampleList = ['TT']
+VarList = ['count']
 
-##
-#RegionList = ['TTel','ZLFel','ZHFel','TTul','ZLFul','ZHFul','TTeh','ZLFeh','ZHFeh','TTuh','ZLFuh','ZHFuh']
-#AT
+###############
+#Xbb dictionnaries
+###############
+
 SamplePath_Xbb = {'TT':'Xbb/Sample/TT.root','ZHbb':'Xbb/Sample/ZHbb.root','DY400':'Xbb/Sample/DYH400.root'}
-#SampleVar_Xbb = {'PU':['puWeight', 50, 0, 2], 'genSign':['sign(genWeight)', 50, 0, 2],'bTag':['bTagWeightCMVAV2_Moriond', 50, 0, 2],'FitCorr':['FitCorr[0]', 50, 0, 2],'Vweight':['DYw[0]', 50, 0, 2],'LepSelSF':['weight_SF_Lepton[0]', 50, 0, 2],'eTrigSF':['eTrigSFWeight_singleEle80[0]', 50, 0, 2],'uTrigSF':['muTrigSFWeight_singlemu[0]', 50, 0, 2]}
+#Variable definitions
+SampleVar_Xbb = {'Vtype':'Vtype_new','lepPt':'vLeptons_new_pt[0]','HT':'lheHT','CMVAmin':'Jet_btagCMVAV2[hJCMVAV2idx[1]]','Vmass':'V_mass','CMVAmax':'Jet_btagCMVAV2[hJCMVAV2idx[0]]','Vpt':'V_pt','HVdPhi':'VHbb::deltaPhi(HCMVAV2_phi,V_phi)','lepRelIso1':'vLeptons_new_relIso04[0]','jetleadpt':'Jet_pt[hJCMVAV2idx[0]]','jetsubleadpt':'Jet_pt[hJCMVAV2idx[1]]','PU':'puWeight','MET':'met_pt','count':'1','Hmass':'HCMVAV2_mass','lepRelIso2':'vLeptons_new_relIso04[0]','nAddJets':'Sum$(Jet_pt>30 && abs(Jet_eta)<5.2 && Jet_puId>=4)'}
+#Kinematic region definitions
 RegionCut_Xbb = {
     'TTel':'(V_new_mass > 10 & (V_new_mass < 75 || V_new_mass > 120) & Jet_btagCMVAV2[hJCMVAV2idx[0]] > 0.9432 & Jet_btagCMVAV2[hJCMVAV2idx[1]] > -0.5884  & (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20))) & (Vtype_new == 1 && (abs(vLeptons_new_eta[0]) >= 1.57 || abs(vLeptons_new_eta[0]) <= 1.44) & (abs(vLeptons_new_eta[1])>=1.57 || abs(vLeptons_new_eta[1])<=1.44) && vLeptons_new_relIso03[0] < 0.15 && vLeptons_new_relIso03[1] < 0.15) & (V_new_pt > 50 & V_new_pt < 150)',
     'ZLFel':'((V_new_mass > 75. && V_new_mass < 105. && Jet_btagCMVAV2[hJCMVAV2idx[0]] < -0.5884 && Jet_btagCMVAV2[hJCMVAV2idx[1]] < -0.5884 && abs(VHbb::deltaPhi(HCMVAV2_reg_phi,V_new_phi)) > 2.5  & (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20))) && V_new_pt > 50) && (HCMVAV2_reg_mass > 90 && HCMVAV2_reg_mass < 150) & (Vtype_new == 1 && (abs(vLeptons_new_eta[0]) >= 1.57 || abs(vLeptons_new_eta[0]) <= 1.44) & (abs(vLeptons_new_eta[1])>=1.57 || abs(vLeptons_new_eta[1])<=1.44) && vLeptons_new_relIso03[0] < 0.15 && vLeptons_new_relIso03[1] < 0.15) & (V_new_pt > 50 & V_new_pt < 150)',
     'ZHFel':'(abs(VHbb::deltaPhi(HCMVAV2_reg_phi,V_new_phi)) > 2.5  & V_new_mass > 85. & V_new_mass < 97. & met_pt < 60 & (HCMVAV2_reg_mass < 90 || HCMVAV2_reg_mass > 150) & Jet_btagCMVAV2[hJCMVAV2idx[0]] > 0.9432 & Jet_btagCMVAV2[hJCMVAV2idx[1]] > -0.5884 &  (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20)) & V_new_pt > 50) & (Vtype_new == 1 && (abs(vLeptons_new_eta[0]) >= 1.57 || abs(vLeptons_new_eta[0]) <= 1.44) & (abs(vLeptons_new_eta[1])>=1.57 || abs(vLeptons_new_eta[1])<=1.44) && vLeptons_new_relIso03[0] < 0.15 && vLeptons_new_relIso03[1] < 0.15) & (V_new_pt > 50 & V_new_pt < 150)',
+    'ZHFeh':'(abs(VHbb::deltaPhi(HCMVAV2_reg_phi,V_new_phi)) > 2.5  & V_new_mass > 85. & V_new_mass < 97. & met_pt < 60 & (HCMVAV2_reg_mass < 90 || HCMVAV2_reg_mass > 150) & Jet_btagCMVAV2[hJCMVAV2idx[0]] > 0.9432 & Jet_btagCMVAV2[hJCMVAV2idx[1]] > -0.5884 &  (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20)) & V_new_pt > 50) & (Vtype_new == 1 && (abs(vLeptons_new_eta[0]) >= 1.57 || abs(vLeptons_new_eta[0]) <= 1.44) & (abs(vLeptons_new_eta[1])>=1.57 || abs(vLeptons_new_eta[1])<=1.44) && vLeptons_new_relIso03[0] < 0.15 && vLeptons_new_relIso03[1] < 0.15) & (V_new_pt > 150)',
     'TTul':'(V_new_mass > 10 & (V_new_mass < 75 || V_new_mass > 120) & Jet_btagCMVAV2[hJCMVAV2idx[0]] > 0.9432 & Jet_btagCMVAV2[hJCMVAV2idx[1]] > -0.5884  & (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20))) & (Vtype_new == 0) & (V_new_pt > 50 & V_new_pt < 150)',
     'ZLFul':'((V_new_mass > 75. && V_new_mass < 105. && Jet_btagCMVAV2[hJCMVAV2idx[0]] < -0.5884 && Jet_btagCMVAV2[hJCMVAV2idx[1]] < -0.5884 && abs(VHbb::deltaPhi(HCMVAV2_reg_phi,V_new_phi)) > 2.5  & (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20))) && V_new_pt > 50) && (HCMVAV2_reg_mass > 90 && HCMVAV2_reg_mass < 150) & (Vtype_new == 0) & (V_new_pt > 50 & V_new_pt < 150)',
     'ZHFul':'(abs(VHbb::deltaPhi(HCMVAV2_reg_phi,V_new_phi)) > 2.5  & V_new_mass > 85. & V_new_mass < 97. & met_pt < 60 & (HCMVAV2_reg_mass < 90 || HCMVAV2_reg_mass > 150) & Jet_btagCMVAV2[hJCMVAV2idx[0]] > 0.9432 & Jet_btagCMVAV2[hJCMVAV2idx[1]] > -0.5884 &  (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20)) & V_new_pt > 50) & (Vtype_new == 0) & (V_new_pt > 50 & V_new_pt < 150)',
@@ -64,6 +64,13 @@ RegionCut_Xbb = {
     'ZHFuh':'(abs(VHbb::deltaPhi(HCMVAV2_reg_phi,V_new_phi)) > 2.5  & V_new_mass > 85. & V_new_mass < 97. & met_pt < 60 & (HCMVAV2_reg_mass < 90 || HCMVAV2_reg_mass > 150) & Jet_btagCMVAV2[hJCMVAV2idx[0]] > 0.9432 & Jet_btagCMVAV2[hJCMVAV2idx[1]] > -0.5884 &  (BasicCutsCMVA && ( hJetCMVAV2_pt_reg_0 > 20 &&  hJetCMVAV2_pt_reg_1 > 20) && (vLeptons_new_pt[0]>20 && vLeptons_new_pt[1]>20)) & V_new_pt > 50) & (Vtype_new == 0) & (V_new_pt > 150)'
     }
 
+###############
+#AT dictionnaries
+###############
+
+SamplePath_AT= {'TT':'AT/Heiner_mail_18_01_2018/CR_2LeptREGION/TT_powheg.root','ZHbb':'AT/Heiner_mail_18_01_2018/CR_2LeptREGION/ZH125_powheg.root','DY400':'AT/Heiner_mail_18_01_2018/CR_2LeptREGION/DYToLL_HT400to600.root'}
+
+SampleVar_AT = {'Vtype':'Vtype','lepPt':'lepPt','HT':'Hpt','CMVAmin':'CMVAmin','Vmass':'Vmass','CMVAmax':'CMVAmax','Vpt':'Vpt','cutFlow':'cutFlow','HVdPhi':'HVdPhi','lepRelIso1':'lepRelIso1','nAddLeptons':'nAddLeptons','lepMetDPhi':'lepMetDPhi','sigma_met_pt':'sigma_met_pt','jetleadpt':'jetleadpt','jetsubleadpt':'jetsubleadpt','':'controlSample','PU':'puWeight','MET':'met_pt','MetTkMetDPhi':'MetTkMetDPhi','count':'count','BDT':'BDT','Hmass':'Hmass','lepRelIso2':'lepRelIso2','nAddJets':'nAddJets','minMetjDPhi':'minMetjDPhi'}
 
 #Analysis Tools
 #Histograms location in files from Heiner
@@ -80,27 +87,6 @@ DirName = {'TTel':'CR_2Lepton_lowVpt_TTbar_El',
     'ZLFuh':'CR_2Lepton_highVpt_VpLF_Mu',
     'ZHFuh':'CR_2Lepton_highVpt_VpHF_Mu'
     }
-
-#SamplePath_AT= {'AT/Heiner_mail_18_01_2018/CR_2Lept','AT/Heiner_mail_18_01_2018/CR_2Lept'}
-
-
-
-SamplePath_AT= {'TT':'AT/Heiner_mail_18_01_2018/CR_2LeptREGION/TT_powheg.root','ZHbb':'AT/Heiner_mail_18_01_2018/CR_2LeptREGION/ZH125_powheg.root','DY400':'AT/Heiner_mail_18_01_2018/CR_2LeptREGION/DYToLL_HT400to600.root'}
-#SampleVar_AT = {'PU':['puWeight', 50, 0, 2]}
-#RegionCut_AT = {'TT':'selLeptons_relIso_0<0.06&&met_pt<170&&isWmunu&&Vtype==2&&controlSample==1'}
-
-
-
-#MY
-#VarList = ['PU','genSign','bTag','dEtabb', 'NLOVpt', 'NLOewk','Wpt','eSelSF','uSelSF','eTrigSF','uTrigSF']
-#Heiner
-#VarList = ['Vtype','lepPt','HT', 'CMVAmin',' Vmass', 'CMVAmax', 'Vpt','cutFlow','HVdPhi','lepRelIso1','nAddLeptons','lepMetDPhi','sigma_met_pt','jetleadpt','jetsubleadpt','controlSample','puWeight','MET','MetTkMetDPhi','count','BDT','Hmass','lepRelIso2','nAddJets','minMetjDPhi']
-#To be used
-#What is the cutFlow var ? count
-#VarList = ['Vtype','lepPt','HT', 'CMVAmin',' Vmass', 'CMVAmax', 'Vpt','HVdPhi','lepRelIso1','nAddLeptons','jetleadpt','jetsubleadpt','puWeight','MET','BDT','Hmass','lepRelIso2','nAddJets', 'count']
-SampleVar_AT = {'Vtype':'Vtype','lepPt':'lepPt','HT':'Hpt','CMVAmin':'CMVAmin','Vmass':'Vmass','CMVAmax':'CMVAmax','Vpt':'Vpt','cutFlow':'cutFlow','HVdPhi':'HVdPhi','lepRelIso1':'lepRelIso1','nAddLeptons':'nAddLeptons','lepMetDPhi':'lepMetDPhi','sigma_met_pt':'sigma_met_pt','jetleadpt':'jetleadpt','jetsubleadpt':'jetsubleadpt','':'controlSample','PU':'puWeight','MET':'met_pt','MetTkMetDPhi':'MetTkMetDPhi','count':'count','BDT':'BDT','Hmass':'Hmass','lepRelIso2':'lepRelIso2','nAddJets':'nAddJets','minMetjDPhi':'minMetjDPhi'}
-##For test
-#SampleVar_AT = {'PU':'puWeight'}
 
 ############
 #Read all the AT histograms
@@ -156,7 +142,7 @@ def MakeRegionDic(regionList, sampleList):
 
 ##For test
 hlist = GetHisto('AT/Heiner_mail_18_01_2018/CR_2LeptLO/DYToLL_HT400to600.root', 'ZLFel')
-print 'hlist is', hlist
+#print 'hlist is', hlist
 #sys.exit()
 #slist = MakeSampleDic('ZLFel', SampleList)
 #print 'slist is', slist
@@ -165,42 +151,50 @@ print 'hlist is', hlist
 
 #Create dic organising all the histograms from AT
 AT_AllHist = MakeRegionDic(RegionList, SampleList)
-#print 'AT_AllHist', AT_AllHist
 
-#sys.exit()
+nevents = 'Region\tSample\tXbb\tAT\n\n'
 
+if not os.path.exists('plots'):
+    os.makedirs('plots')
+    subprocess.call('cp -r .htaccess ' + 'plots/', shell = True)
+    subprocess.call('cp -r index.php ' + 'plots/', shell = True)
 
 #To check variables in common subregion
-
 for region in RegionList:
+
+    nevents += region + '\t\n'
+
     print '========='
     print region
     print '========='
+    #
+    #Create folder for each region if doesn't exist
+    #
+    output_path = region
+    if not os.path.exists('plots/'+region):
+        os.makedirs('plots/'+region)
+        subprocess.call('cp -r .htaccess ' + 'plots/'+region + '/', shell = True)
+        subprocess.call('cp -r index.php ' + 'plots/'+region + '/', shell = True)
+
+    #To write the yield information
+
     for sample in SampleList:
+
+        nevents += '\t'+sample+'\t'
+        Xbbcount = -1
+        ATcount = -1
+
         #Xbb
         file_Xbb = ROOT.TFile.Open(SamplePath_Xbb[sample],'read')
         tree_Xbb = file_Xbb.Get('tree')
         print tree_Xbb.GetName()
         ##AT
-        #file_AT = ROOT.TFile.Open(SamplePath_AT[sample],'read')
-        #tree_AT = file_AT.Get('tree')
-        #print tree_AT.GetName()
 
-        #Count the number of entries
-        n_Xbb =  tree_Xbb.GetEntries(RegionCut_Xbb[region])
-        #n_AT = tree_AT.GetEntries(RegionCut_AT[region])
-
-        #print '\n'
-        #print sample
-        #print 'Xbb yield:', n_Xbb
-        #print 'AT  yield:', n_AT
-        #print '\n'
-
+        ##Count the number of entries
+        #n_Xbb =  tree_Xbb.GetEntries(RegionCut_Xbb[region])
 
         #Make plots for all the relevant variables
         for var in VarList:
-
-
 
             #print '-------------------'
             #print AT_AllHist[region][sample]
@@ -209,9 +203,6 @@ for region in RegionList:
             nbins= h_AT.GetNbinsX()
             xmin = h_AT.GetXaxis().GetBinLowEdge(1)
             xmax = h_AT.GetXaxis().GetBinLowEdge(nbins+1)
-
-            #h_AT = ROOT.TH1F('h_AT','h_AT',SampleVar_AT[var][1], SampleVar_AT[var][2], SampleVar_AT[var][3])
-            #tree_AT.Draw('%s>>%s' %(SampleVar_AT[var][0], 'h_AT'), RegionCut_AT[region])
 
             h_Xbb = ROOT.TH1F('h_Xbb','h_Xbb', nbins, xmin, xmax)
             tree_Xbb.Draw('%s>>%s' %(SampleVar_Xbb[var], 'h_Xbb'), RegionCut_Xbb[region])
@@ -222,25 +213,40 @@ for region in RegionList:
             h_Xbb.Draw()
             h_Xbb.SetLineColor(2)
             h_Xbb.SetLineWidth(4)
+
+            Xbb_count_ = h_Xbb.Integral()
             if not h_Xbb.Integral() == 0:
-                h_Xbb.Scale(1./h_Xbb.Integral())
+                h_Xbb.Scale(1./Xbb_count_)
             h_AT.Draw('SAME')
             h_AT.SetLineColor(4)
             h_AT.SetLineStyle(2)
             h_AT.SetLineWidth(4)
-            if not h_AT.Integral() == 0:
-                h_AT.Scale(1./h_AT.Integral())
 
-            leg = ROOT.TLegend(0.1,0.7,0.48,0.9)
+            AT_count_ = h_AT.Integral()
+            if not h_AT.Integral() == 0:
+                h_AT.Scale(1./AT_count_)
+
+            leg = ROOT.TLegend(0,0.8,0.15,1)
             leg.AddEntry(h_Xbb, 'Xbb')
             leg.AddEntry(h_AT, 'AT')
             leg.Draw()
 
-            c.SaveAs('%s_%s_%s.pdf'%(region, sample,var))
-            c.SaveAs('%s_%s_%s.png'%(region, sample,var))
-            c.SaveAs('%s_%s_%s.root'%(region,sample,var))
+            #Max histogram value
+            ymax = max(h_Xbb.GetMaximum(),h_AT.GetMaximum())
+            h_Xbb.GetYaxis().SetRangeUser(0,ymax*1.2)
 
 
+            c.SaveAs('plots/%s/%s_%s_%s.pdf'%(output_path,region, sample,var))
+            c.SaveAs('plots/%s/%s_%s_%s.png'%(output_path,region, sample,var))
+            c.SaveAs('plots/%s/%s_%s_%s.root'%(output_path,region,sample,var))
 
+            if var == 'count':
+                Xbbcount = Xbb_count_
+                ATcount = AT_count_
+        nevents += str(Xbbcount) + '\t'
+        nevents += str(ATcount)+ '\t\n'
 
+f = open('plots/yields.txt', 'w')
+f.write(nevents)
+f.close
 
