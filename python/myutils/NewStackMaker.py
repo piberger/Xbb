a16febf80326880d60ae6f42567ceac602c64ff5
piberger <pberger@phys.ethz.ch>
2018-02-02 13:14:31 +0100

    plot histogram for signals for all variables to separate root file

--- python/myutils/NewStackMaker.py
+++ python/myutils/NewStackMaker.py
@@ -396,6 +396,23 @@ class NewStackMaker:
         if self.is2D:
             drawOption = self.histogramOptions['drawOption'] if 'drawOption' in self.histogramOptions else 'colz'
 
+        # save histogram of all added signal samples as TH1 object to separate .root file
+        fileName = self.outputFileTemplate.format(outputFolder=outputFolder, prefix=prefix + '_SIGNAL_HISTOGRAM', prefixSeparator='_' if len(prefix)>0 else '', var=self.var+'_SIGNAL_HISTOGRAM', ext='root')
+        tf1 = ROOT.TFile.Open(fileName, 'recreate')
+        print("INFO: write histogram to:", fileName)
+        clonedHist = None
+        for histogram in self.histograms:
+            if histogram['group'] in ['ZHbb']:
+                if not clonedHist:
+                    clonedHist = histogram['histogram'].Clone('zh_and_ggzh')
+                else:
+                    clonedHist.Add(histogram['histogram'])
+        if clonedHist:
+            clonedHist.SetDirectory(tf1)
+        tf1.Write()
+        tf1.Close()
+
+
         # draw stack/sum
         if self.is2D:
             self.plotLabels['topright1'] = {'text': '2Dplot', 'x': 0.69, 'y': 0.9, 'size': 0.7}
