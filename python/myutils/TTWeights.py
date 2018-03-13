21ddd3178811ca76e20c8782deb4391da2d3c03f
piberger <pberger@phys.ethz.ch>
2018-03-13 16:37:58 +0100

    fix in tt weights for nano

--- python/myutils/TTWeights.py
+++ python/myutils/TTWeights.py
@@ -13,9 +13,9 @@ class TTWeights(object):
         self.sampleTree = initVars['sampleTree']
         if self.sample.isMC():
             if self.nano:
-                self.sampleTree.addFormula("nTop", "Sum$(GenPart_pdgId==6&&GenPart_genPartIdxMother==0)")
-                self.sampleTree.addFormula("top0_pt","MinIf$(GenPart_pt,GenPart_pdgId==6&&GenPart_genPartIdxMother==0)")
-                self.sampleTree.addFormula("top1_pt","MaxIf$(GenPart_pt,GenPart_pdgId==6&&GenPart_genPartIdxMother==0)")
+                self.sampleTree.addFormula("nTop", "Sum$(GenPart_pdgId==6)")
+                self.sampleTree.addFormula("top0_pt","MinIf$(GenPart_pt,GenPart_pdgId==6)")
+                self.sampleTree.addFormula("top1_pt","MaxIf$(GenPart_pt,GenPart_pdgId==6)")
             else:
                 self.sampleTree.addFormula("nTop", "nGenTop")
                 self.sampleTree.addFormula("top0_pt","GenTop_pt[0]")
@@ -27,7 +27,7 @@ class TTWeights(object):
 
     def getTTW(self, tree):
         TTW = 1
-        if self.sampleTree.evaluate("nTop") == 2:
+        if self.sampleTree.evaluate("nTop") == 2 and not 'ST_' in self.sample.identifier:
             sf_top1 = math.exp(0.0615 - 0.0005*self.sampleTree.evaluate("top0_pt"))
             sf_top2 = math.exp(0.0615 - 0.0005*self.sampleTree.evaluate("top1_pt"))
             TTW = math.sqrt(sf_top1*sf_top2)
