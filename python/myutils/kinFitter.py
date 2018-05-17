6e71e35680f9b569582cb90cf6dd38fe140a6d61
piberger <pberger@phys.ethz.ch>
2018-05-17 02:25:58 +0200

    adapt kinFit wrapper to NanoAOD, add configs for kin fit for 2016

--- python/myutils/kinFitter.py
+++ python/myutils/kinFitter.py
@@ -25,48 +25,80 @@ class NullTree(object):
 
 class AnalysisToolsTree(object):
     def __init__(self, sampleTree, isData=False):
-
-        self.formulas = {
-                'hJetInd1': 'hJCMVAV2idx[0]',
-                'hJetInd2': 'hJCMVAV2idx[1]',
-                'hj1_pt': 'Jet_pt[hJCMVAV2idx[0]]',
-                'hj1_eta': 'Jet_eta[hJCMVAV2idx[0]]',
-                'hj1_phi': 'Jet_phi[hJCMVAV2idx[0]]',
-                'hj1_mass': 'Jet_mass[hJCMVAV2idx[0]]',
-                'hj2_pt': 'Jet_pt[hJCMVAV2idx[1]]',
-                'hj2_eta': 'Jet_eta[hJCMVAV2idx[1]]',
-                'hj2_phi': 'Jet_phi[hJCMVAV2idx[1]]',
-                'hj2_mass': 'Jet_mass[hJCMVAV2idx[1]]',
-                'GenBJ1_eta': '-1' if isData else 'Jet_mcEta[hJCMVAV2idx[0]]', 
-                'GenBJ2_eta': '-1' if isData else 'Jet_mcEta[hJCMVAV2idx[1]]', 
-                'GenBJ1_phi': '-1' if isData else 'Jet_mcPhi[hJCMVAV2idx[0]]', 
-                'GenBJ2_phi': '-1' if isData else 'Jet_mcPhi[hJCMVAV2idx[1]]', 
-                'GenBJ1_pt': '-1' if isData else 'Jet_mcPt[hJCMVAV2idx[0]]', 
-                'GenBJ2_pt': '-1' if isData else 'Jet_mcPt[hJCMVAV2idx[1]]', 
-                'GenBJ1_mass': '-1' if isData else 'Jet_mcM[hJCMVAV2idx[0]]', 
-                'GenBJ2_mass': '-1' if isData else 'Jet_mcM[hJCMVAV2idx[1]]', 
-                'isZmm': '(Vtype==0)',
-                'lepInd1': '0',
-                'lepInd2': '1',
-                }
-        self.aliases = {
-                'Jet_Pt': 'Jet_pt',
-                'Jet_bReg': 'Jet_pt_reg',
-                'n_hj_matched': 'nhJCMVAV2idx',
-                'fixedGridRhoFastjetAll': 'rho',
-                'Jet_jetId': 'Jet_id',
-                'Jet_lepFilter': 'Jet_pt',
-                'Muon_pt': 'vLeptons_new_pt',
-                'Muon_eta': 'vLeptons_new_eta',
-                'Muon_phi': 'vLeptons_new_phi',
-                'Muon_mass': 'vLeptons_new_mass',
-                'Electron_pt': 'vLeptons_new_pt',
-                'Electron_eta': 'vLeptons_new_eta',
-                'Electron_phi': 'vLeptons_new_phi',
-                'Electron_mass': 'vLeptons_new_mass',
-                }
-        self.castToInt = {'hJetInd1': True, 'hJetInd2': True, 'lepInd1': True, 'lepInd2':True}
-        self.constantArray = {'Muon_ptErr': [2, 9.0], 'Electron_energyErr': [2, 25.0]}
+        
+        if sampleTree.treeName == 'tree':
+            self.formulas = {
+                    'hJetInd1': 'hJCMVAV2idx[0]',
+                    'hJetInd2': 'hJCMVAV2idx[1]',
+                    'hj1_pt': 'Jet_pt[hJCMVAV2idx[0]]',
+                    'hj1_eta': 'Jet_eta[hJCMVAV2idx[0]]',
+                    'hj1_phi': 'Jet_phi[hJCMVAV2idx[0]]',
+                    'hj1_mass': 'Jet_mass[hJCMVAV2idx[0]]',
+                    'hj2_pt': 'Jet_pt[hJCMVAV2idx[1]]',
+                    'hj2_eta': 'Jet_eta[hJCMVAV2idx[1]]',
+                    'hj2_phi': 'Jet_phi[hJCMVAV2idx[1]]',
+                    'hj2_mass': 'Jet_mass[hJCMVAV2idx[1]]',
+                    'GenBJ1_eta': '-1' if isData else 'Jet_mcEta[hJCMVAV2idx[0]]', 
+                    'GenBJ2_eta': '-1' if isData else 'Jet_mcEta[hJCMVAV2idx[1]]', 
+                    'GenBJ1_phi': '-1' if isData else 'Jet_mcPhi[hJCMVAV2idx[0]]', 
+                    'GenBJ2_phi': '-1' if isData else 'Jet_mcPhi[hJCMVAV2idx[1]]', 
+                    'GenBJ1_pt': '-1' if isData else 'Jet_mcPt[hJCMVAV2idx[0]]', 
+                    'GenBJ2_pt': '-1' if isData else 'Jet_mcPt[hJCMVAV2idx[1]]', 
+                    'GenBJ1_mass': '-1' if isData else 'Jet_mcM[hJCMVAV2idx[0]]', 
+                    'GenBJ2_mass': '-1' if isData else 'Jet_mcM[hJCMVAV2idx[1]]', 
+                    'isZmm': '(Vtype==0)',
+                    'lepInd1': '0',
+                    'lepInd2': '1',
+                    }
+            self.aliases = {
+                    'Jet_Pt': 'Jet_pt',
+                    'Jet_bReg': 'Jet_pt_reg',
+                    'n_hj_matched': 'nhJCMVAV2idx',
+                    'fixedGridRhoFastjetAll': 'rho',
+                    'Jet_jetId': 'Jet_id',
+                    'Jet_lepFilter': 'Jet_pt',
+                    'Muon_pt': 'vLeptons_new_pt',
+                    'Muon_eta': 'vLeptons_new_eta',
+                    'Muon_phi': 'vLeptons_new_phi',
+                    'Muon_mass': 'vLeptons_new_mass',
+                    'Electron_pt': 'vLeptons_new_pt',
+                    'Electron_eta': 'vLeptons_new_eta',
+                    'Electron_phi': 'vLeptons_new_phi',
+                    'Electron_mass': 'vLeptons_new_mass',
+                    }
+            self.castToInt = {'hJetInd1': True, 'hJetInd2': True, 'lepInd1': True, 'lepInd2':True}
+            self.constantArray = {'Muon_ptErr': [2, 9.0], 'Electron_energyErr': [2, 25.0]}
+        else:
+            # NANOAOD
+            self.formulas = {
+                    'hJetInd1': 'hJidx[0]',
+                    'hJetInd2': 'hJidx[1]',
+                    'hj1_pt': 'Jet_Pt[hJidx[0]]',
+                    'hj1_eta': 'Jet_eta[hJidx[0]]',
+                    'hj1_phi': 'Jet_phi[hJidx[0]]',
+                    'hj1_mass': 'Jet_mass[hJidx[0]]',
+                    'hj2_pt': 'Jet_Pt[hJidx[1]]',
+                    'hj2_eta': 'Jet_eta[hJidx[1]]',
+                    'hj2_phi': 'Jet_phi[hJidx[1]]',
+                    'hj2_mass': 'Jet_mass[hJidx[1]]',
+                    'GenBJ1_eta': '-1' if isData else '-1', #'GenJet_eta[Jet_genJetIdx[hJidx[0]]]',
+                    'GenBJ2_eta': '-1' if isData else '-1', #'GenJet_eta[Jet_genJetIdx[hJidx[1]]]',
+                    'GenBJ1_phi': '-1' if isData else '-1', #'GenJet_phi[Jet_genJetIdx[hJidx[0]]]',
+                    'GenBJ2_phi': '-1' if isData else '-1', #'GenJet_phi[Jet_genJetIdx[hJidx[1]]]',
+                    'GenBJ1_pt': '-1' if isData else '-1', #'GenJet_pt[Jet_genJetIdx[hJidx[0]]]',
+                    'GenBJ2_pt': '-1' if isData else '-1', #'GenJet_pt[Jet_genJetIdx[hJidx[1]]]',
+                    'GenBJ1_mass': '-1' if isData else '-1', #'GenJet_mass[Jet_genJetIdx[hJidx[0]]]',
+                    'GenBJ2_mass': '-1' if isData else '-1', #'GenJet_mass[Jet_genJetIdx[hJidx[1]]]',
+                    'isZmm': '(Vtype==0)',
+                    'lepInd1': '(nVMuonIdx==2&&nVElectronIdx<2)*Alt$(VMuonIdx[0],0) + (nVMuonIdx<2&&nVElectronIdx==2)*Alt$(VElectronIdx[0],0)',
+                    'lepInd2': '(nVMuonIdx==2&&nVElectronIdx<2)*Alt$(VMuonIdx[1],0) + (nVMuonIdx<2&&nVElectronIdx==2)*Alt$(VElectronIdx[1],0)',
+                    'GenLepIndex1': '-1',
+                    'GenLepIndex2': '-1',
+                    }
+            self.aliases = {
+                    }
+            self.castToInt = {'hJetInd1': True, 'hJetInd2': True, 'lepInd1': True, 'lepInd2':True, 'GenLepIndex1': True, 'GenLepIndex2': True}
+            self.constantArray = {}
         
         self.sampleTree = sampleTree
         for alias, target in self.aliases.iteritems():
@@ -88,9 +120,10 @@ class AnalysisToolsTree(object):
 
 class kinFitter(AddCollectionsModule):
 
-    def __init__(self, branchName="kinFit"):
+    def __init__(self, branchName="kinFit", skipBadEvents=False):
         super(kinFitter, self).__init__()
         self.branchName = branchName
+        self.skipBadEvents = skipBadEvents
         
         self.leaves = VarContainer(NullTree()).tree_vars
         self.kinFitterCollection = Collection(self.branchName, self.leaves, leaves=True) 
@@ -116,7 +149,15 @@ class kinFitter(AddCollectionsModule):
             
             # run kinematic fitter
             c = VarContainer(NullTree())
-            apply_fit_to_event(self.wrappedTree, c, self.ak4pfchs_ptres)
+            try:
+                apply_fit_to_event(self.wrappedTree, c, self.ak4pfchs_ptres)
+            except Exception as e:
+                print "\x1b[31mEXCEPTION:",e,"\x1b[0m"
+                if self.skipBadEvents:
+                    return False
+                else:
+                    raise Exception("KinFitException")
+
 
             # write output branches
             for i, leaf in enumerate(self.leaves):
