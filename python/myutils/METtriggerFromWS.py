bba8eb51a058841266da914027fc3b1c5cb53ef5
piberger <pberger@phys.ethz.ch>
2018-07-10 12:01:30 +0200

    improvements to MET trigger sf script

--- python/myutils/METtriggerFromWS.py
+++ python/myutils/METtriggerFromWS.py
@@ -7,23 +7,26 @@ import os
 
 class METtriggerFromWS(AddCollectionsModule):
 
-    def __init__(self, workspace="data/met/vhbb_metsf.root", fName="data_nominal_110OR170"):
+    def __init__(self, branchName="weight_mettrigSF", workspace="data/met/vhbb_metsf.root", fName="data_nominal_110OR170"):
         self.debug = 'XBBDEBUG' in os.environ
         self.workspaceFileName = workspace
         self.fName = fName
+        self.branchName = branchName
         super(METtriggerFromWS, self).__init__()
 
     def loadWorkspace(self, wsName):
         rootFile = ROOT.TFile.Open(wsName, "READ")
         wsp = rootFile.Get("w")
-        wsp.Print()
+        if self.debug:
+            print "DEBUG: loaded workspace:"
+            wsp.Print()
         rootFile.Close()
         self.met_trigger_sf = wsp.function(self.fName).functor(ROOT.RooArgList(wsp.argSet("met_mht")))
 
     def customInit(self, initVars):
         self.isData = initVars['sample'].isData()
         if not self.isData:
-            self.addBranch('weight_mettrigSF', 1.0)
+            self.addBranch(self.branchName, 1.0)
             self.loadWorkspace(self.workspaceFileName)
     
     def processEvent(self, tree):
@@ -31,6 +34,6 @@ class METtriggerFromWS(AddCollectionsModule):
             self.markProcessed(tree)
 
             met_mht = array.array('d', [min(min(tree.MET_pt, tree.MHT_pt), 500.0)])
-            self.setBranch('weight_mettrigSF', self.met_trigger_sf.eval(met_mht[0]) if met_mht[0] >= 120 else 0.0)
+            self.setBranch(self.branchName, self.met_trigger_sf.eval(met_mht) if met_mht[0] >= 120 else 0.0)
 
         return True
