93163c6683497a4833f828a38edfd44ca7f23cb8
Gael Perrin <gael.ludovic.perrin@cern.ch>
2018-07-26 14:50:35 +0200

    Included new variables for Wlv nano 2016 analysis

--- python/myutils/GetWTMass.py
+++ python/myutils/GetWTMass.py
@@ -5,10 +5,12 @@ import array
 from pdgId import pdgId
 import sys
 import math as m
+import pdb
 
 class GetWTMass(object):
 
-    def __init__(self, sample=None):
+    def __init__(self, sample=None, nano=False):
+        self.nano = nano
         self.lastEntry = -1
         self.branchBuffers = {}
         self.branches = []
@@ -37,11 +39,18 @@ class GetWTMass(object):
             Lep = ROOT.TLorentzVector()
             MET = ROOT.TLorentzVector()
 
-            Lep.SetPtEtaPhiM(tree.vLeptons_new_pt[0], tree.vLeptons_new_eta[0], tree.vLeptons_new_phi[0], tree.vLeptons_new_mass[0])
-            MET.SetPtEtaPhiM(tree.met_pt, tree.met_eta, tree.met_phi, tree.met_mass)
+            if not self.nano:
+                MET.SetPtEtaPhiM(tree.met_pt, tree.met_eta, tree.met_phi, tree.met_mass)
+                Lep.SetPtEtaPhiM(tree.vLeptons_new_pt[0], tree.vLeptons_new_eta[0], tree.vLeptons_new_phi[0], tree.vLeptons_new_mass[0])
+            else:
+                MET.SetPtEtaPhiM(tree.MET_pt, 0, tree.MET_phi, 0)
+                if len(getattr(tree,'VMuonIdx')) == 1:
+                    Lep.SetPtEtaPhiM(tree.Muon_pt[tree.VMuonIdx[0]], tree.Muon_eta[tree.VMuonIdx[0]], tree.Muon_phi[tree.VMuonIdx[0]], tree.Muon_mass[tree.VMuonIdx[0]])
+                if len(getattr(tree,'VElectronIdx')) == 1:
+                    Lep.SetPtEtaPhiM(tree.Electron_pt[tree.VElectronIdx[0]], tree.Electron_eta[tree.VElectronIdx[0]], tree.Electron_phi[tree.VElectronIdx[0]], tree.Electron_mass[tree.VElectronIdx[0]])
 
 
-            cosPhi12 = (Lep.Px()*MET.Px() + Lep.Py()*MET.Py()) / (Lep.Pt() * MET.Pt())
+            cosPhi12 = min(1,(Lep.Px()*MET.Px() + Lep.Py()*MET.Py()) / (Lep.Pt() * MET.Pt()))
             self.branchBuffers['V_mt'][0] = m.sqrt(2*Lep.Pt()*MET.Pt() * (1 - cosPhi12))
 
             return True
