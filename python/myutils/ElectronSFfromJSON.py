4b9812784e533515ca64176e43c3ed82584e8c95
piberger <pberger@phys.ethz.ch>
2018-08-07 17:14:43 +0200

    fix tabs in electron SF script, add option for branch name

--- python/myutils/ElectronSFfromJSON.py
+++ python/myutils/ElectronSFfromJSON.py
@@ -3,12 +3,15 @@ import os
 import array
 from JsonTable import JsonTable
 from vLeptons import vLeptonSelector
+from BranchTools import Collection
+from BranchTools import AddCollectionsModule
 
-class ElectronSFfromJSON(object):
+class ElectronSFfromJSON(AddCollectionsModule):
     
-    def __init__(self, jsonFiles=None):
+    def __init__(self, jsonFiles=None, branchName="electronSF"):
         self.jsonFiles = jsonFiles
         self.debug = 'XBBDEBUG' in os.environ
+        self.branchName = branchName
 
         # load JOSN files
         self.jsonTable = JsonTable(jsonFiles)
@@ -28,25 +31,15 @@ class ElectronSFfromJSON(object):
         self.branchBuffers = {}
         self.lastEntry = -1
         if not self.isData:
-            for branchName in ['electronSF', 'electronSF_IdIso', 'electronSF_trigger']:
-		self.branchBuffers[branchName] = array.array('f', [1.0, 1.0, 1.0])
+            for branchName in [self.branchName, self.branchName + '_IdIso', self.branchName + '_trigger']:
+                self.branchBuffers[branchName] = array.array('f', [1.0, 1.0, 1.0])
                 self.branches.append({'name': branchName, 'formula': self.getVectorBranch, 'arguments': {'branch': branchName, 'length':3}, 'length': 3})
-            for branchName in ['electronSF_IdIso_B', 'electronSF_IdIso_E']:
+            for branchName in [self.branchName + '_IdIso_B', self.branchName + '_IdIso_E']:
                 self.branchBuffers[branchName] = array.array('f', [1.0, 1.0])
                 self.branches.append({'name': branchName, 'formula': self.getVectorBranch, 'arguments': {'branch': branchName, 'length':2}, 'length': 2})
-#                for syst in self.systVariations:
-#                    branchNameSyst = branchName+syst if syst else branchName
-#                    self.branchBuffers[branchNameSyst] = array.array('f', [0])
-#                    self.branches.append({'name': branchNameSyst, 'formula': self.getBranch, 'arguments': branchNameSyst}) 
 
     def getBranches(self):
         return self.branches
-
-    # read from buffers which have been filled in processEvent()    
-    def getBranch(self, event, arguments=None):
-        self.processEvent(event)
-        if arguments:
-            return self.branchBuffers[arguments][0]
 
     def getVectorBranch(self, event, arguments=None, destinationArray=None):
         self.processEvent(event)
@@ -54,102 +47,81 @@ class ElectronSFfromJSON(object):
             destinationArray[i] =  self.branchBuffers[arguments['branch']][i]
 
     def processEvent(self, tree):
-        currentEntry = tree.GetReadEntry()
         # if current entry has not been processed yet
-        if currentEntry != self.lastEntry and not self.isData:
-            self.lastEntry = currentEntry
+        if not self.hasBeenProcessed(tree) and not self.isData:
+            self.markProcessed(tree)
 
             zElectrons = vLeptonSelector(tree, config=self.config).getZelectrons()
+            lep_eta = []
+            lep_pt = []
+            if len(zElectrons) == 2: 
+                lep_eta = [zElectrons[0].eta,zElectrons[1].eta]
+                lep_pt = [zElectrons[0].pt,zElectrons[1].pt]
 
-       	    lep_eta = []
-	    lep_pt = []
-	    if len(zElectrons) == 2: 
-       	     lep_eta = [zElectrons[0].eta,zElectrons[1].eta]
-	     lep_pt = [zElectrons[0].pt,zElectrons[1].pt]
-
-            self.computeSF(self.branchBuffers['electronSF_trigger'], self.branchBuffers['electronSF_IdIso'], self.branchBuffers['electronSF_IdIso_B'], self.branchBuffers['electronSF_IdIso_E'], self.branchBuffers['electronSF'], lep_eta, lep_pt, len(zElectrons), 1.566)
-
-
-#            for syst in self.systVariations:
-#                # require two electrons
-#                if len(zElectrons) == 2: 
-#                    sfIdIso = self.getIdIsoSf(eta=zElectrons[0].eta, pt=zElectrons[0].pt, syst=syst) * self.getIdIsoSf(eta=zElectrons[1].eta, pt=zElectrons[1].pt, syst=syst)
-#                    sfTrigger = self.getTriggerSf(eta1=zElectrons[0].eta, pt1=zElectrons[0].pt, eta2=zElectrons[1].eta, pt2=zElectrons[1].pt, syst=syst)
-#                else:
-#                    sfIdIso = 1.0
-#                    sfTrigger = 1.0
-#                # Up/Down variations should be taken from individual components instead of total during the datacard creation
-#                self.branchBuffers['electronSF' + (syst if syst else '')][0] = sfIdIso * sfTrigger
-#                self.branchBuffers['electronSF_IdIso' + (syst if syst else '')][0] = sfIdIso
-#                self.branchBuffers['electronSF_trigger' + (syst if syst else '')][0] = sfTrigger
+            self.computeSF(self.branchBuffers[self.branchName + '_trigger'], self.branchBuffers[self.branchName + '_IdIso'], self.branchBuffers[self.branchName + '_IdIso_B'], self.branchBuffers[self.branchName + '_IdIso_E'], self.branchBuffers[self.branchName], lep_eta, lep_pt, len(zElectrons), 1.566)
 
         return True
 
 
 #Separated weight_Iso in eta (Barrel/Endcap) :
 #----------------------------------------------------------------------------------------------------
-	 
     def computeSF(self, weight_trigg, weight_Iso, weight_Iso_LowEta, weight_Iso_HighEta, weight_SF, lep_eta, lep_pt, lep_n, etacut):
-
         '''Computes the trigger, IdIso (including separated variations in eta) and final event SF'''
         # require two electrons
-#	print "Number of leptons {nl}".format(nl=lep_n)
-	if lep_n == 2:
+        if lep_n == 2:
 
- 	 #Calculating IdIso weights, separately for each electron
-	 weight = []
-	 for lep in [0,1]:
-	  #weights_temp=[Nominal,Down,Up] for the corespondong lepton	 
-	  weights_temp=[]
-          for syst in self.systVariations:
-	   weights_temp.append(self.getIdIsoSf(eta=lep_eta[lep], pt=lep_pt[lep], syst=syst))
-	  weight.append(weights_temp)
+            #Calculating IdIso weights, separately for each electron
+            weight = []
+            for lep in [0,1]:
+                weights_temp = []
+                for syst in self.systVariations:
+                    weights_temp.append(self.getIdIsoSf(eta=lep_eta[lep], pt=lep_pt[lep], syst=syst))
+                weight.append(weights_temp)
 
-	 #Calculating the trigger and IdIso weights and Down/Up variations
-         for i, syst in enumerate(self.systVariations):
-	  weight_trigg[i] = self.getTriggerSf(eta1=lep_eta[0], pt1=lep_pt[0], eta2=lep_eta[1], pt2=lep_pt[1], syst=syst)
-	  weight_Iso[i] = weight[0][i] * weight[1][i]
-	  weight_SF[i] = weight_trigg[i] * weight_Iso[i]
+            #Calculating the trigger and IdIso weights and Down/Up variations
+            for i, syst in enumerate(self.systVariations):
+                weight_trigg[i] = self.getTriggerSf(eta1=lep_eta[0], pt1=lep_pt[0], eta2=lep_eta[1], pt2=lep_pt[1], syst=syst)
+                weight_Iso[i] = weight[0][i] * weight[1][i]
+                weight_SF[i] = weight_trigg[i] * weight_Iso[i]
 
-	 #Assignes the IdIso for different eta regions (Barrel : LowEta and Endcap : Higheta)
-         if abs(lep_eta[0]) < etacut and abs(lep_eta[1]) < etacut:
-             #assign sys
-             weight_Iso_LowEta[0] = weight[0][1] * weight[1][1]
-             weight_Iso_LowEta[1] = weight[0][2] * weight[1][2]
-             #sys are nom value
-             weight_Iso_HighEta[0] = weight[0][0] * weight[1][0]
-             weight_Iso_HighEta[1] = weight[0][0] * weight[1][0]
+            #Assignes the IdIso for different eta regions (Barrel : LowEta and Endcap : Higheta)
+            if abs(lep_eta[0]) < etacut and abs(lep_eta[1]) < etacut:
+                #assign sys
+                weight_Iso_LowEta[0] = weight[0][1] * weight[1][1]
+                weight_Iso_LowEta[1] = weight[0][2] * weight[1][2]
+                #sys are nom value
+                weight_Iso_HighEta[0] = weight[0][0] * weight[1][0]
+                weight_Iso_HighEta[1] = weight[0][0] * weight[1][0]
 
-         elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) > etacut:
-             #sys are nom value
-             weight_Iso_LowEta[0] = weight[0][0] * weight[1][0]
-             weight_Iso_LowEta[1] = weight[0][0] * weight[1][0]
-             #assign sys
-             weight_Iso_HighEta[0] =  weight[0][1] * weight[1][1]
-             weight_Iso_HighEta[1] =  weight[0][2] * weight[1][2]
+            elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) > etacut:
+                #sys are nom value
+                weight_Iso_LowEta[0] = weight[0][0] * weight[1][0]
+                weight_Iso_LowEta[1] = weight[0][0] * weight[1][0]
+                #assign sys
+                weight_Iso_HighEta[0] =  weight[0][1] * weight[1][1]
+                weight_Iso_HighEta[1] =  weight[0][2] * weight[1][2]
 
-         elif abs(lep_eta[0]) < etacut and abs(lep_eta[1]) > etacut:
-             weight_Iso_LowEta[0] =  weight[0][1] * weight[1][0]
-             weight_Iso_LowEta[1] =  weight[0][2] * weight[1][0]
-             weight_Iso_HighEta[0] = weight[0][0] * weight[1][1]
-             weight_Iso_HighEta[1] = weight[0][0] * weight[1][2]
+            elif abs(lep_eta[0]) < etacut and abs(lep_eta[1]) > etacut:
+                weight_Iso_LowEta[0] =  weight[0][1] * weight[1][0]
+                weight_Iso_LowEta[1] =  weight[0][2] * weight[1][0]
+                weight_Iso_HighEta[0] = weight[0][0] * weight[1][1]
+                weight_Iso_HighEta[1] = weight[0][0] * weight[1][2]
 
-         elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) < etacut:
-             weight_Iso_LowEta[0] = weight[0][0] * weight[1][1]
-             weight_Iso_LowEta[1] = weight[0][0] * weight[1][2]
-             weight_Iso_HighEta[0] = weight[0][1] * weight[1][0]
-             weight_Iso_HighEta[1] = weight[0][2] * weight[1][0]
-	#This is when an event has more than 2 lepton
-	else:
-         for i, syst in enumerate(self.systVariations):
-	  weight_trigg[i] = 1.
-	  weight_Iso[i] = 1.
-	  weight_SF[i] = 1.
-	  if i <= 1:
-	   weight_Iso_LowEta[i] = 1.0
-	   weight_Iso_HighEta[i] = 1.0
+            elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) < etacut:
+                weight_Iso_LowEta[0] = weight[0][0] * weight[1][1]
+                weight_Iso_LowEta[1] = weight[0][0] * weight[1][2]
+                weight_Iso_HighEta[0] = weight[0][1] * weight[1][0]
+                weight_Iso_HighEta[1] = weight[0][2] * weight[1][0]
+        #This is when an event has more than 2 lepton
+        else:
+            for i, syst in enumerate(self.systVariations):
+                weight_trigg[i] = 1.
+                weight_Iso[i] = 1.
+                weight_SF[i] = 1.
+                if i <= 1:
+                    weight_Iso_LowEta[i] = 1.0
+                    weight_Iso_HighEta[i] = 1.0
 
-	 
 #-------------------------------------------------------------------------------------------------------
 
     def getIdIsoSf(self, eta, pt, syst=None):
