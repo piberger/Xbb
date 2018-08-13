304b023e129b5723fb7dc3edc401e27cee667348
Gael Perrin <gael.ludovic.perrin@cern.ch>
2018-08-13 14:38:41 +0200

    fixed bug in MakeSysList

--- python/myscripts/MakeSysList.py
+++ python/myscripts/MakeSysList.py
@@ -8,7 +8,7 @@ ROOT.gStyle.SetOptStat(0)
 ROOT.gROOT.SetBatch()
 
 #nominal
-VarList_in = 'SYS_UD: H_mass_SYS_UD,H_pt_SYS_UD,V_pt,abs(TVector2::Phi_mpi_pi(H_phi-V_phi)),Jet_btagCMVA[hJidxCMVA[1]],MET,abs(TVector2::Phi_mpi_pi(Alt$(Muon_phi[tree.VMuonIdx[0]],0)+Alt$(Electron_phi[tree.VElectronIdx[0]],0)-V_phi)),top_mass,V_mt,Sum$(Jet_pt_SYSUD>25&&abs(Jet_eta)<2.5&&Jet_puId>0&&Jet_lepFilter&&Iteration$!=<!General|btagidx0!>&&Iteration$!=<!General|btagidx1!>),SA5'
+VarList_in = 'SYS_UD: H_mass_SYS_UD H_pt_SYS_UD V_pt abs(TVector2::Phi_mpi_pi(H_phi-V_phi)) Jet_btagCMVA[hJidxCMVA[1]] MET_pt_SYSUD abs(TVector2::Phi_mpi_pi(Alt$(Muon_phi[VMuonIdx[0]],0)+Alt$(Electron_phi[VElectronIdx[0]],0)-V_phi)) top_mass_SYS_UD V_mt Sum$(Jet_pt_SYSUD>25&&abs(Jet_eta)<2.5&&Jet_puId>0&&Jet_lepFilter&&Iteration$!=<!General|btagidx0!>&&Iteration$!=<!General|btagidx1!>) SA5'
 
 #List of the systematics
 JECsys = [
@@ -44,7 +44,7 @@ JECsys = [
     ]
 
 #List of the variables affected by the sys
-VarSysDic= ['H_mass_SYS_UD','H_pt_SYS_UD','Sum$(Jet_pt_SYSUD>25&&abs(Jet_eta)<2.5&&Jet_puId>0&&Jet_lepFilter&&Iteration$!=<!General|btagidx0!>&&Iteration$!=<!General|btagidx1!>)']
+#VarSysDic= ['H_mass_SYS_UD','H_pt_SYS_UD', 'top_mass_SYS_UD','Sum$(Jet_pt_SYSUD>25&&abs(Jet_eta)<2.5&&Jet_puId>0&&Jet_lepFilter&&Iteration$!=<!General|btagidx0!>&&Iteration$!=<!General|btagidx1!>)']
 
 VarList_new =  []
 SysList = JECsys
@@ -52,15 +52,22 @@ UDList = ['Up','Down']
 for syst in SysList:
     for ud in UDList:
         VarList = VarList_in
-        for var in VarSysDic:
-            if not var in VarList:
-                print 'var:', var, 'not found'
-                print 'Aborting'
-                sys.exit()
-            VarList = VarList.replace(var,var+"_corr"+syst+ud)
+        #for var in VarSysDic:
+        #    if not var in VarList:
+        #        print 'var:', var, 'not found'
+        #        print 'Aborting'
+        #        sys.exit()
+        #    VarList = VarList.replace(var,var+"_corr"+syst+ud)
         VarList = VarList.replace('SYS',syst)
-        VarList = VarList.replace('_UD','_'+ud)
+        VarList = VarList.replace('UD',ud)
         VarList_new.append(VarList)
+
+print '==========================='
+print 'PRINTING Nominal'
+print '==========================='
+VarList_nominal = VarList_in
+VarList_nominal = VarList_nominal.replace('_SYS_UD','').replace('_SYSUD','')
+print VarList_nominal
 
 print '==========================='
 print 'PRINTING varlists'
