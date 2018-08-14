696f1dee93f948f31a77a229a8ae07f1fa1e7fb3
Vladimir Ohanesjan (ETHZ) [vlohanes] <ohanesjv@phys.ethz.ch>
2018-08-14 19:38:33 +0200

    included Wlv channel in the lepton weights modules

--- python/myutils/ElectronSFfromJSON.py
+++ python/myutils/ElectronSFfromJSON.py
@@ -8,18 +8,31 @@ from BranchTools import AddCollectionsModule
 
 class ElectronSFfromJSON(AddCollectionsModule):
     
-    def __init__(self, jsonFiles=None, branchName="electronSF"):
+    def __init__(self, jsonFiles=None, branchName="electronSF",channel='None'):
         self.jsonFiles = jsonFiles
         self.debug = 'XBBDEBUG' in os.environ
         self.branchName = branchName
 
         # load JOSN files
         self.jsonTable = JsonTable(jsonFiles)
-        self.idIsoSfName = 'doubleEleIDISO2017'
-        self.triggerLegNames = ['doubleEleTriggerLeg1', 'doubleEleTriggerLeg2']
+        self.channel = channel 
+        if self.channel== 'Zll':
+         self.idIsoSfName = 'doubleEleIDISO2017'
+         self.triggerLegNames = ['doubleEleTriggerLeg1', 'doubleEleTriggerLeg2']
+        elif self.channel == 'Wlv':
+         self.idIsoSfName = 'singleEleIDISO2017'
+         self.triggerLegNames = ['singleEleTrigger']
+        else: 
+         print "Channel not defined!"
+         quit()
+
         self.systVariations = [None, 'Down', 'Up']
         self.idIsoSf = self.jsonTable.getEtaPtTable(self.idIsoSfName)
         self.triggerSf = [self.jsonTable.getEtaPtTable(x) for x in self.triggerLegNames]
+#        print "Channel is: {}".format(self.channel)
+#        print "ISO files: {}".format(self.idIsoSfName)
+#        print "Trigg files: {}".format(self.triggerLegNames)
+#        quit()
 
     def customInit(self, initVars):
         sample = initVars['sample']
@@ -51,14 +64,37 @@ class ElectronSFfromJSON(AddCollectionsModule):
         if not self.hasBeenProcessed(tree) and not self.isData:
             self.markProcessed(tree)
 
-            zElectrons = vLeptonSelector(tree, config=self.config).getZelectrons()
+#TODO check the assigned weights for events with more than 2 letpns
+            Vtype = tree.Vtype
+            vLidx = []
             lep_eta = []
             lep_pt = []
-            if len(zElectrons) == 2: 
-                lep_eta = [zElectrons[0].eta,zElectrons[1].eta]
-                lep_pt = [zElectrons[0].pt,zElectrons[1].pt]
+         
+            if self.channel == "Wlv" and Vtype == 3:
+               vLidx = [tree.vLidx[0]]
+               lep_pt = [tree.Electron_pt[vLidx[0]]]
+               lep_eta = [tree.Electron_eta[vLidx[0]]]
+            elif self.channel == "Zll" and Vtype ==1:
+               vLidx = [tree.vLidx[0],tree.vLidx[1]]
+               lep_pt = [tree.Electron_pt[vLidx[0]],tree.Electron_pt[vLidx[1]]]
+               lep_eta = [tree.Electron_eta[vLidx[0]],tree.Electron_eta[vLidx[1]]]
+#            print "Vtype: ", Vtype 
+#            print "idx: ", vLidx 
+#            print "pt: ", lep_pt 
+#            print "eta: ", lep_eta 
 
-            self.computeSF(self.branchBuffers[self.branchName + '_trigger'], self.branchBuffers[self.branchName + '_IdIso'], self.branchBuffers[self.branchName + '_IdIso_B'], self.branchBuffers[self.branchName + '_IdIso_E'], self.branchBuffers[self.branchName], lep_eta, lep_pt, len(zElectrons), 1.566)
+#Old version
+#            lep_eta = []
+#            lep_pt = []
+#            if len(vElectrons) == 1: 
+#                lep_eta = [vElectrons[0].eta]
+#                lep_pt = [vElectrons[0].pt]
+#            elif len(vElectrons) == 2: 
+#                lep_eta = [vElectrons[0].eta,vElectrons[1].eta]
+#                lep_pt = [vElectrons[0].pt,vElectrons[1].pt]
+#            print " number of electrons" len(vElectrons)
+
+            self.computeSF(self.branchBuffers[self.branchName + '_trigger'], self.branchBuffers[self.branchName + '_IdIso'], self.branchBuffers[self.branchName + '_IdIso_B'], self.branchBuffers[self.branchName + '_IdIso_E'], self.branchBuffers[self.branchName], lep_eta, lep_pt, len(vLidx), 1.566)
 
         return True
 
@@ -68,11 +104,11 @@ class ElectronSFfromJSON(AddCollectionsModule):
     def computeSF(self, weight_trigg, weight_Iso, weight_Iso_LowEta, weight_Iso_HighEta, weight_SF, lep_eta, lep_pt, lep_n, etacut):
         '''Computes the trigger, IdIso (including separated variations in eta) and final event SF'''
         # require two electrons
-        if lep_n == 2:
 
+        if lep_n == 1 or lep_n == 2:
             #Calculating IdIso weights, separately for each electron
             weight = []
-            for lep in [0,1]:
+            for lep in range(lep_n):
                 weights_temp = []
                 for syst in self.systVariations:
                     weights_temp.append(self.getIdIsoSf(eta=lep_eta[lep], pt=lep_pt[lep], syst=syst))
@@ -80,39 +116,13 @@ class ElectronSFfromJSON(AddCollectionsModule):
 
             #Calculating the trigger and IdIso weights and Down/Up variations
             for i, syst in enumerate(self.systVariations):
-                weight_trigg[i] = self.getTriggerSf(eta1=lep_eta[0], pt1=lep_pt[0], eta2=lep_eta[1], pt2=lep_pt[1], syst=syst)
-                weight_Iso[i] = weight[0][i] * weight[1][i]
-                weight_SF[i] = weight_trigg[i] * weight_Iso[i]
+               weight_trigg[i] = self.getTriggerSf(lep_eta, lep_pt, lep_n, syst=syst)
+               weight_Iso[i] = weight[0][i] if lep_n == 1 else weight[0][i] * weight[1][i] 
+               weight_SF[i] = weight_trigg[i] * weight_Iso[i]
+            #Barrel and Endcap separated
+            self.electronSf_BE(weight, weight_Iso_LowEta, weight_Iso_HighEta, lep_eta, lep_pt, lep_n, etacut)
 
-            #Assignes the IdIso for different eta regions (Barrel : LowEta and Endcap : Higheta)
-            if abs(lep_eta[0]) < etacut and abs(lep_eta[1]) < etacut:
-                #assign sys
-                weight_Iso_LowEta[0] = weight[0][1] * weight[1][1]
-                weight_Iso_LowEta[1] = weight[0][2] * weight[1][2]
-                #sys are nom value
-                weight_Iso_HighEta[0] = weight[0][0] * weight[1][0]
-                weight_Iso_HighEta[1] = weight[0][0] * weight[1][0]
-
-            elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) > etacut:
-                #sys are nom value
-                weight_Iso_LowEta[0] = weight[0][0] * weight[1][0]
-                weight_Iso_LowEta[1] = weight[0][0] * weight[1][0]
-                #assign sys
-                weight_Iso_HighEta[0] =  weight[0][1] * weight[1][1]
-                weight_Iso_HighEta[1] =  weight[0][2] * weight[1][2]
-
-            elif abs(lep_eta[0]) < etacut and abs(lep_eta[1]) > etacut:
-                weight_Iso_LowEta[0] =  weight[0][1] * weight[1][0]
-                weight_Iso_LowEta[1] =  weight[0][2] * weight[1][0]
-                weight_Iso_HighEta[0] = weight[0][0] * weight[1][1]
-                weight_Iso_HighEta[1] = weight[0][0] * weight[1][2]
-
-            elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) < etacut:
-                weight_Iso_LowEta[0] = weight[0][0] * weight[1][1]
-                weight_Iso_LowEta[1] = weight[0][0] * weight[1][2]
-                weight_Iso_HighEta[0] = weight[0][1] * weight[1][0]
-                weight_Iso_HighEta[1] = weight[0][2] * weight[1][0]
-        #This is when an event has more than 2 lepton
+        #This is when an event has 0 or more than 2 lepton
         else:
             for i, syst in enumerate(self.systVariations):
                 weight_trigg[i] = 1.
@@ -124,21 +134,82 @@ class ElectronSFfromJSON(AddCollectionsModule):
 
 #-------------------------------------------------------------------------------------------------------
 
+    def electronSf_BE(self, weight, weight_Iso_LowEta, weight_Iso_HighEta, lep_eta, lep_pt, lep_n, etacut):
+        '''Assignes the IdIso for different eta regions (Barrel : LowEta and Endcap : Higheta)'''
+
+        #Used for the Wlv channel
+        if lep_n == 1:
+          if abs(lep_eta[0]) < etacut:
+              #assign sys
+              weight_Iso_LowEta[0] = weight[0][1] 
+              weight_Iso_LowEta[1] = weight[0][2]
+              #sys are nom value
+              weight_Iso_HighEta[0] = weight[0][0]
+              weight_Iso_HighEta[1] = weight[0][0]
+          elif abs(lep_eta[0]) > etacut:
+              #sys are nom value
+              weight_Iso_LowEta[0] = weight[0][0]
+              weight_Iso_LowEta[1] = weight[0][0]
+              #assign sys
+              weight_Iso_HighEta[0] =  weight[0][1]
+              weight_Iso_HighEta[1] =  weight[0][2]
+ 
+        #Used for the Zll channel
+        if lep_n == 2:
+          if abs(lep_eta[0]) < etacut and abs(lep_eta[1]) < etacut:
+              #assign sys
+              weight_Iso_LowEta[0] = weight[0][1] * weight[1][1]
+              weight_Iso_LowEta[1] = weight[0][2] * weight[1][2]
+              #sys are nom value
+              weight_Iso_HighEta[0] = weight[0][0] * weight[1][0]
+              weight_Iso_HighEta[1] = weight[0][0] * weight[1][0]
+ 
+          elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) > etacut:
+              #sys are nom value
+              weight_Iso_LowEta[0] = weight[0][0] * weight[1][0]
+              weight_Iso_LowEta[1] = weight[0][0] * weight[1][0]
+              #assign sys
+              weight_Iso_HighEta[0] =  weight[0][1] * weight[1][1]
+              weight_Iso_HighEta[1] =  weight[0][2] * weight[1][2]
+ 
+          elif abs(lep_eta[0]) < etacut and abs(lep_eta[1]) > etacut:
+              weight_Iso_LowEta[0] =  weight[0][1] * weight[1][0]
+              weight_Iso_LowEta[1] =  weight[0][2] * weight[1][0]
+              weight_Iso_HighEta[0] = weight[0][0] * weight[1][1]
+              weight_Iso_HighEta[1] = weight[0][0] * weight[1][2]
+ 
+          elif abs(lep_eta[0]) > etacut and abs(lep_eta[1]) < etacut:
+              weight_Iso_LowEta[0] = weight[0][0] * weight[1][1]
+              weight_Iso_LowEta[1] = weight[0][0] * weight[1][2]
+              weight_Iso_HighEta[0] = weight[0][1] * weight[1][0]
+              weight_Iso_HighEta[1] = weight[0][2] * weight[1][0]
+
+
     def getIdIsoSf(self, eta, pt, syst=None):
         sf = self.jsonTable.find(self.idIsoSf, eta, pt, syst=syst)
         #if self.debug:
         #    print "id/iso eta:", eta, "pt:", pt, "->", sf
         return sf
     
-    def getTriggerSf(self, eta1, pt1, eta2, pt2, syst=None):
-        leg1 = self.jsonTable.find(self.triggerSf[0], eta1, pt1, syst=syst)
-    
-        leg2 = self.jsonTable.find(self.triggerSf[1], eta2, pt2, syst=syst)
-        #if self.debug:
-        #    print "leg1: eta:", eta1, " pt:", pt1, "->", leg1
-        #    print "leg2: eta:", eta2, " pt:", pt2, "->", leg2
-        #    print "-->", leg1*leg2
-        return leg1*leg2
+#    def getTriggerSf(self, eta1, pt1, eta2, pt2, syst=None):
+#        leg1 = self.jsonTable.find(self.triggerSf[0], eta1, pt1, syst=syst)
+#    
+#        leg2 = self.jsonTable.find(self.triggerSf[1], eta2, pt2, syst=syst)
+#        #if self.debug:
+#        #    print "leg1: eta:", eta1, " pt:", pt1, "->", leg1
+#        #    print "leg2: eta:", eta2, " pt:", pt2, "->", leg2
+#        #    print "-->", leg1*leg2
+#        return leg1*leg2
+
+    def getTriggerSf(self, eta, pt, len_n, syst=None):
+        triggSF = 1.
+        for i in range(len_n):
+#         leg[i] = self.jsonTable.find(self.triggerSf[i], eta[i], pt[i], syst=syst)
+         triggSF = triggSF * self.jsonTable.find(self.triggerSf[i], eta[i], pt[i], syst=syst)
+#         print "leg", i, " : eta:", eta[i], " pt:", pt[i], "->", self.jsonTable.find(self.triggerSf[i], eta[i], pt[i], syst=syst)
+
+#        print "triggSF:", triggSF
+        return triggSF
 
 if __name__ == "__main__":
     sfObject = ElectronSFfromJSON([
--- python/myutils/MuonSFfromJSON.py
+++ python/myutils/MuonSFfromJSON.py
@@ -6,14 +6,27 @@ from vLeptons import vLeptonSelector
 
 class MuonSFfromJSON(object):
 
-    def __init__(self, jsonFiles=None):
+    def __init__(self, jsonFiles=None, branchName='muonSF',channel='None'):
         self.jsonFiles = jsonFiles
         self.debug = 'XBBDEBUG' in os.environ
+        self.branchName = branchName
 
         # load JOSN files
+
         self.jsonTable = JsonTable(jsonFiles)
-        self.idSf = self.jsonTable.getEtaPtTable('NUM_LooseID_DEN_genTracks', 'abseta_pt')
-        self.isoSf = self.jsonTable.getEtaPtTable('NUM_LooseRelIso_DEN_LooseID', 'abseta_pt')
+        self.channel = channel 
+        if self.channel== 'Zll':
+         self.idSf = self.jsonTable.getEtaPtTable('NUM_LooseID_DEN_genTracks', 'abseta_pt')
+         self.isoSf = self.jsonTable.getEtaPtTable('NUM_LooseRelIso_DEN_LooseID', 'abseta_pt')
+#         self.triggerSf = ; not implemented yet
+        elif self.channel == 'Wlv':
+         self.idSf = self.jsonTable.getEtaPtTable('NUM_TightID_DEN_genTracks', 'abseta_pt')
+         self.isoSf = self.jsonTable.getEtaPtTable('NUM_UltraTightIso4_DEN_TightIDandIPCut', 'abseta_pt')
+         self.triggerSf = [self.jsonTable.getEtaPtTable('NUM_IsoMu27_DEN_empty', 'abseta_pt')]
+        else: 
+         print "Channel not defined!"
+         quit()
+        self.systVariations = [None, 'Down', 'Up']
 
     def customInit(self, initVars):
         sample = initVars['sample']
@@ -25,12 +38,17 @@ class MuonSFfromJSON(object):
         self.branchBuffers = {}
         self.lastEntry = -1
         if not self.isData:
-            for branchName in ['muonSF', 'muonSF_Id', 'muonSF_Iso', 'muonSF_trigger']:
+            for branchName in [self.branchName, self.branchName + '_Id', self.branchName + '_Iso', self.branchName + '_trigger']:
                 self.branchBuffers[branchName] = array.array('f', [1.0, 1.0, 1.0])
-                self.branches.append({'name': branchName, 'formula': self.getBranch, 'arguments': branchName}) 
+                self.branches.append({'name': branchName, 'formula': self.getVectorBranch, 'arguments': {'branch': branchName, 'length':3}, 'length': 3})
 
     def getBranches(self):
         return self.branches
+
+    def getVectorBranch(self, event, arguments=None, destinationArray=None):
+        self.processEvent(event)
+        for i in range(arguments['length']):
+            destinationArray[i] =  self.branchBuffers[arguments['branch']][i]
 
     # read from buffers which have been filled in processEvent()    
     def getBranch(self, event, arguments=None):
@@ -44,62 +62,109 @@ class MuonSFfromJSON(object):
         if currentEntry != self.lastEntry and not self.isData:
             self.lastEntry = currentEntry
 
-            vLeptons = vLeptonSelector(tree, config=self.config)
-            zMuons = vLeptons.getZmuons()
+#TODO check the assigned weights for events with more than 2 letpns
+            Vtype = tree.Vtype
+            vLidx = []
+            lep_eta = []
+            lep_pt = []
+         
+            if self.channel == "Wlv" and Vtype == 2:
+               vLidx = [tree.vLidx[0]]
+               lep_pt = [tree.Muon_pt[vLidx[0]]]
+               lep_eta = [tree.Muon_eta[vLidx[0]]]
+            elif self.channel == "Zll" and Vtype ==0:
+               vLidx = [tree.vLidx[0],tree.vLidx[1]]
+               lep_pt = [tree.Muon_pt[vLidx[0]],tree.Muon_pt[vLidx[1]]]
+               lep_eta = [tree.Muon_eta[vLidx[0]],tree.Muon_eta[vLidx[1]]]
 
-            # require two muons
-            if len(zMuons) == 2:
-                sfId =       self.getIdSf(eta=zMuons[0].eta, pt=zMuons[0].pt) * self.getIdSf(eta=zMuons[1].eta, pt=zMuons[1].pt) 
-                sfId_Down =  self.getIdSf(eta=zMuons[0].eta, pt=zMuons[0].pt) * self.getIdSf(eta=zMuons[1].eta, pt=zMuons[1].pt, syst='Down') 
-                sfId_Up =    self.getIdSf(eta=zMuons[0].eta, pt=zMuons[0].pt) * self.getIdSf(eta=zMuons[1].eta, pt=zMuons[1].pt, syst='Up') 
-                
-                sfIso =      self.getIsoSf(eta=zMuons[0].eta, pt=zMuons[0].pt) * self.getIsoSf(eta=zMuons[1].eta, pt=zMuons[1].pt) 
-                sfIso_Down = self.getIsoSf(eta=zMuons[0].eta, pt=zMuons[0].pt) * self.getIsoSf(eta=zMuons[1].eta, pt=zMuons[1].pt, syst='Down') 
-                sfIso_Up =   self.getIsoSf(eta=zMuons[0].eta, pt=zMuons[0].pt) * self.getIsoSf(eta=zMuons[1].eta, pt=zMuons[1].pt, syst='Up') 
+#Old version
+#            vLeptons = vLeptonSelector(tree, config=self.config)
+#
+#            if self.channel == "Wlv":
+#             vMuons = vLeptons.getWmuons()
+#            if self.channel == "Zll":
+#             vMuons = vLeptons.getZmuons()
+#
+#            lep_eta = []
+#            lep_pt = []
+#            if len(vMuons) == 1: 
+#                lep_eta = [vMuons[0].eta]
+#                lep_pt = [vMuons[0].pt]
+#            elif len(vMuons) == 2: 
+#                lep_eta = [vMuons[0].eta,vMuons[1].eta]
+#                lep_pt = [vMuons[0].pt,vMuons[1].pt]
+#            print " number of electrons" len(vMuons)
 
-                sfTrigger =      self.getTriggerSf(eta1=zMuons[0].eta, pt1=zMuons[0].pt, eta2=zMuons[1].eta, pt2=zMuons[1].pt)
-                sfTrigger_Down = self.getTriggerSf(eta1=zMuons[0].eta, pt1=zMuons[0].pt, eta2=zMuons[1].eta, pt2=zMuons[1].pt, syst='Down')
-                sfTrigger_Up =   self.getTriggerSf(eta1=zMuons[0].eta, pt1=zMuons[0].pt, eta2=zMuons[1].eta, pt2=zMuons[1].pt, syst='Up')
+            self.computeSF(self.branchBuffers[self.branchName + '_trigger'], self.branchBuffers[self.branchName + '_Id'], self.branchBuffers[self.branchName + '_Iso'], self.branchBuffers[self.branchName], lep_eta, lep_pt, len(vLidx))
 
-                self.branchBuffers['muonSF'][0] = sfId * sfIso * sfTrigger
-                self.branchBuffers['muonSF'][1] = sfId_Down * sfIso_Down * sfTrigger_Down
-                self.branchBuffers['muonSF'][2] = sfId_Up * sfIso_Up * sfTrigger_Up
-                self.branchBuffers['muonSF_Id'][0] = sfId
-                self.branchBuffers['muonSF_Id'][1] = sfId_Down
-                self.branchBuffers['muonSF_Id'][2] = sfId_Up
-                self.branchBuffers['muonSF_Iso'][0] = sfIso
-                self.branchBuffers['muonSF_Iso'][1] = sfIso_Down
-                self.branchBuffers['muonSF_Iso'][2] = sfIso_Up
-                self.branchBuffers['muonSF_trigger'][0] = sfTrigger
-                self.branchBuffers['muonSF_trigger'][1] = sfTrigger_Down
-                self.branchBuffers['muonSF_trigger'][2] = sfTrigger_Up
-            else:
-                for i in range(3):
-                    self.branchBuffers['muonSF'][i] = 1.0
-                    self.branchBuffers['muonSF_Id'][i] = 1.0
-                    self.branchBuffers['muonSF_Iso'][i] = 1.0
-                    self.branchBuffers['muonSF_trigger'][i] = 1.0
         return True
 
-    def getIdSf(self, eta, pt, syst=None):
-        return self.jsonTable.find(self.idSf, eta=eta, pt=pt, syst=syst)
+    def computeSF(self, weight_trigg, weight_Id, weight_Iso, weight_SF, lep_eta, lep_pt, lep_n):
+        '''Computes the trigger, IdIso (including separated variations in eta) and final event SF'''
+        # require two electrons
 
-    def getIsoSf(self, eta, pt, syst=None):
-        return self.jsonTable.find(self.isoSf, eta=eta, pt=pt, syst=syst)
-    
-    def getTriggerSf(self, eta1, pt1, eta2, pt2, syst=None):
-        leg1 = 1.0 #not implemented yet
-        leg2 = 1.0 
-        #define efficiency for MC and 
-        effData_leg8 = []
-        effData_leg17= []
-        effMC_leg8= []
-        effMC_leg17 = []
-        #if self.debug:
-        #    print "leg1: eta:", eta1, " pt:", pt1, "->", leg1
-        #    print "leg2: eta:", eta2, " pt:", pt2, "->", leg2
-        #    print "-->", leg1*leg2
-        return leg1*leg2
+        if lep_n == 1 or lep_n == 2:
+
+            #Calculating the trigger and IdIso weights and Down/Up variations
+            for i, syst in enumerate(self.systVariations):
+               weight_trigg[i] = self.getTriggerSf(lep_eta, lep_pt, lep_n, syst=syst)
+               weight_Id[i] = self.getIdSf(lep_eta, lep_pt, lep_n)
+               weight_Iso[i] = self.getIsoSf(lep_eta, lep_pt, lep_n)
+               weight_SF[i] = weight_trigg[i] * weight_Id[i] * weight_Iso[i]
+
+#               print "trigg", weight_trigg[i] 
+#               print "Id", weight_Id[i]
+#               print "Iso", weight_Iso[i]
+#               print "SF", weight_SF[i]
+
+        #This is when an event has 0 or more than 2 lepton
+        else:
+            for i, syst in enumerate(self.systVariations):
+                weight_trigg[i] = 1.
+                weight_Id[i] = 1.
+                weight_Iso[i] = 1.
+                weight_SF[i] = 1.
+
+    def getIdSf(self, eta, pt, len_n, syst=None):
+        idSF = 1.
+        for i in range(len_n):
+            idSF = idSF * self.jsonTable.find(self.idSf, eta[i], pt[i], syst=syst)
+
+        return idSF
+
+
+    def getIsoSf(self, eta, pt, len_n, syst=None):
+        isoSF = 1.
+        for i in range(len_n):
+            isoSF = isoSF * self.jsonTable.find(self.isoSf, eta=eta[i], pt=pt[i], syst=syst)
+
+        return isoSF
+          
+  
+#    def getTriggerSf(self, eta1, pt1, eta2, pt2, syst=None):
+#        leg1 = 1.0 #not implemented yet
+#        leg2 = 1.0 
+#        #define efficiency for MC and 
+#        effData_leg8 = []
+#        effData_leg17= []
+#        effMC_leg8= []
+#        effMC_leg17 = []
+#        #if self.debug:
+#        #    print "leg1: eta:", eta1, " pt:", pt1, "->", leg1
+#        #    print "leg2: eta:", eta2, " pt:", pt2, "->", leg2
+#        #    print "-->", leg1*leg2
+#        return leg1*leg2
+
+    def getTriggerSf(self, eta, pt, len_n, syst=None):
+        triggSF = 1.
+        if self.channel == 'Zll' :
+           trigSF = 1. #not implemented for Zll2017 (when ready, just remove the if/else block)
+        else: 
+           for i in range(len_n):
+            triggSF = triggSF * self.jsonTable.find(self.triggerSf[i], eta[i], pt[i], syst=syst)
+            #print "leg", i, " : eta:", eta[i], " pt:", pt[i], "->", self.jsonTable.find(self.triggerSf[i], eta[i], pt[i], syst=syst)
+
+        return triggSF
 
 if __name__ == "__main__":
     sfObject = MuonSFfromJSON([
