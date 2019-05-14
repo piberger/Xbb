701a96df55ee18b672e58e5f299e0f67de8cbbe5
piberger <pberger@phys.ethz.ch>
2019-05-14 12:05:22 +0200

    clean up fit correction module, add flag to apply the fit correction for W+jets and single top only in 1-lepton for better sync with AT

--- python/myutils/addFitCorr.py
+++ python/myutils/addFitCorr.py
@@ -1,107 +1,74 @@
 #!/usr/bin/env python
+from __future__ import print_function
 import ROOT
 import numpy as np
 import array
+from BranchTools import Collection
+from BranchTools import AddCollectionsModule
 
-class addFitCorr(object):
+class addFitCorr(AddCollectionsModule):
 
-    def __init__(self, sample=None, nano=False):
-
+    # AT behavior is to correct W+jets AND ST only in the 1-lepton channel.
+    def __init__(self, sample=None, nano=False, branchName='FitCorr', correctWjetsOnlyInOneLepton=False):
         self.nano = nano
-        self.branchBuffers = {}
-        self.branches = []
-        self.branchBuffers['FitCorr'] = np.ones(3, dtype=np.float32)
-        self.branches.append({'name': 'FitCorr', 'formula': self.getVectorBranch, 'arguments': {'branch': 'FitCorr', 'length':3}, 'length': 3})
-
-        if sample:
-            self.customInit(sample=sample)
+        self.branchName = branchName
+        self.correctWjetsOnlyInOneLepton = correctWjetsOnlyInOneLepton
+        super(addFitCorr, self).__init__()
 
     def customInit(self, initVars):
-        sample = initVars['sample']
+        self.sample = initVars['sample']
         self.config = initVars['config']
-        
-        self.dataset = self.config.get('General','dataset')
-        self.isData = sample.type == 'DATA'
-        #print 'sample type is', sample.type
-        self.identifier = sample.identifier
-        #print 'sample id is', sample.identifier
-        if self.isData:
-            self.branches = []
+        self.dataset = self.config.get('General', 'dataset')
 
-    # read from buffers which have been filled in processEvent()    
-    def getVectorBranch(self, event, arguments=None, destinationArray=None):
-        self.processEvent(event)
-        #print 'arguments are', arguments
-        for i in range(arguments['length']):
-            destinationArray[i] =  self.branchBuffers[arguments['branch']][i]
+        if not self.sample.isData():
+            self.addVectorBranch(self.branchName, 1.0, length=3)
+            
+            self.corr = {
+                    '2016': {
+                        'TT':  lambda V_pt: [1.064 - 0.000380*V_pt, 1.064 - 0.000469*V_pt, 1.064 - 0.000291*V_pt],
+                        'WHF': lambda V_pt: [1.259 - 0.00167*V_pt, 1.259 - 0.00180*V_pt, 1.259 - 0.00154*V_pt],
+                        'WLF': lambda V_pt: [1.097 - 0.000575*V_pt, 1.097 - 0.000621*V_pt, 1.097 - 0.000529*V_pt],
+                        },
+                    '2017': {
+                        'TT':  lambda V_pt: [1.103 - 0.00061*V_pt, 1.103 - 0.00069*V_pt, 1.103 - 0.00053*V_pt],
+                        'WHF': lambda V_pt: [1.337 - 0.0016*V_pt, 1.337 - 0.0017*V_pt, 1.337 - 0.0015*V_pt],
+                        'WLF': lambda V_pt: [1.115 - 0.00064*V_pt, 1.115 - 0.00068*V_pt, 1.115 - 0.00060*V_pt],
+                        }
+                    }
 
-    def getBranches(self):
-        return self.branches
+            if self.dataset not in self.corr:
+                print('\x1b[31mERROR: unknown dataset:', self.dataset, '\x1b[0m')
+                raise Exception('UnknownDataset')
+
+    def countBs(self, tree):
+        count = 0
+        if not self.nano:
+            for i in range(tree.nGenJet):
+                if tree.GenJet_pt[i]>20 and abs(tree.GenJet_eta[i])<2.4 and  tree.GenJet_numBHadrons[i] > 0:
+                    count += 1
+        else:
+            for i in range(tree.nGenJet):
+                if tree.GenJet_pt[i]>20 and abs(tree.GenJet_eta[i])<2.4 and  tree.GenJet_hadronFlavour[i]==5:
+                    count += 1
+        return count
+
 
     def processEvent(self, tree):
-        if self.isData:
-            return True 
-        else:
-            CorrFactor = [1, 1, 1]
+        if not self.hasBeenProcessed(tree) and not self.sample.isData():
+            self.markProcessed(tree)
 
-            if not self.nano:
-                V_pt = tree.V_new_pt
-            else:
-                V_pt = tree.V_pt
+            V_pt = tree.V_pt if self.nano else tree.V_new_pt
 
-            if 'TT' in self.identifier:
-                if self.dataset == '2016':
-                    CorrFactor =  [1.064 - 0.000380*V_pt, 1.064 - 0.000469*V_pt, 1.064 - 0.000291*V_pt]
-                elif self.dataset== '2017':
-                    CorrFactor =  [1.103 - 0.00061*V_pt, 1.103 - 0.00069*V_pt, 1.103 - 0.00053*V_pt]
-                    #print "Data set is 2017"
-                else:
-                    print("\x1b[31mERROR: unknown dataset:",dataset,"\x1b[0m")
-            elif ('WJets' in self.identifier or 'WBJets' in self.identifier ):
-                count = 0
-                if not self.nano:
-                    for i in range(tree.nGenJet):
-                        if tree.GenJet_pt[i]>20 and abs(tree.GenJet_eta[i])<2.4 and  tree.GenJet_numBHadrons[i] > 0:
-                            count += 1
-                else:
-                    for i in range(tree.nGenJet):
-                        #'Sum$(GenJet_pt>20 && abs(GenJet_eta)<2.4 && GenJet_hadronFlavour==5)>=2']
-                        if tree.GenJet_pt[i]>20 and abs(tree.GenJet_eta[i])<2.4 and  tree.GenJet_hadronFlavour[i]==5:
-                            count += 1
+            cat = None
+            if self.sample.identifier.startswith('TT'):
+                cat = 'TT'
+            elif (not self.correctWjetsOnlyInOneLepton or (tree.isWmunu or tree.isWenu)):
+                if self.sample.identifier.startswith('WJets') or self.sample.identifier.startswith('WBJets'):
+                    cat = 'WLF' if self.countBs(tree) < 1 else 'WHF'
+                elif self.sample.identifier.startswith('ST'):
+                    # no dedicated ST category, use W+HF
+                    cat = 'WHF'
 
-                # Fix
-                if count >= 1: # Whf
-                    if self.dataset == '2016':
-                        CorrFactor = [1.259 - 0.00167*V_pt, 1.259 - 0.00180*V_pt, 1.259 - 0.00154*V_pt]
-                    elif self.dataset == '2017':
-                        CorrFactor = [1.337 - 0.0016*V_pt, 1.337 - 0.0017*V_pt, 1.337 - 0.0015*V_pt]
-                        #print "Data set is 2017"
-                    else:
-                        print("\x1b[31mERROR: unknown dataset:",dataset,"\x1b[0m")
-                else: #Wlf
-                    if self.dataset == '2016':
-                        CorrFactor = [1.097 - 0.000575*V_pt, 1.097 - 0.000621*V_pt, 1.097 - 0.000529*V_pt]
-                    elif self.dataset == '2017':
-                        CorrFactor = [1.115 - 0.00064*V_pt, 1.115 - 0.00068*V_pt, 1.115 - 0.00060*V_pt]
-                        #print "Data set is 2017"
-                    else:
-                        print("\x1b[31mERROR: unknown dataset:",dataset,"\x1b[0m")
-                # BUG
-                #if count >= 1:
-                #    CorrFactor = [1.097 - 0.000575*V_pt, 1.097 - 0.000621*V_pt, 1.097 - 0.000529*V_pt]
-                #else:
-                #    CorrFactor = [1.259 - 0.00167*V_pt, 1.259 - 0.00180*V_pt, 1.259 - 0.00154*V_pt]
-            elif 'ST' in self.identifier:
-                if self.dataset == '2016':
-                     CorrFactor = [1.259 - 0.00167*V_pt, 1.259 - 0.00180*V_pt, 1.259 - 0.00154*V_pt]
-                elif self.dataset == '2017':
-                     CorrFactor = [1.337 - 0.0016*V_pt, 1.337 - 0.0017*V_pt, 1.337 - 0.0015*V_pt]
-                     #print "Data set is 2017"
-                else:
-                    print("\x1b[31mERROR: unknown dataset:",dataset,"\x1b[0m")
+            self._b(self.branchName)[:] = array.array('d', self.corr[self.dataset][cat](V_pt) if cat else [1.0, 1.0, 1.0])
 
-            self.branchBuffers['FitCorr'][0] = CorrFactor[0]
-            self.branchBuffers['FitCorr'][1] = CorrFactor[1]
-            self.branchBuffers['FitCorr'][2] = CorrFactor[2]
-            #print 'CorrFactor is', CorrFactor
         return True 
