c265329bd1f15e1f370da9beaa9f94db55edd7c4
piberger <pberger@phys.ethz.ch>
2019-06-14 21:58:16 +0200

    add standalone kin-fitter code, add DNN input config with kinfit+reg vars, add JME resolution helper

---
+++ KinFitter.h
@@ -0,0 +1,25 @@
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ defined_in "HelperClasses/KinFitter/TAbsFitConstraint.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TAbsFitParticle.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitConstraintEp.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitConstraintM.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitConstraintMGaus.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleCart.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleECart.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleEMomDev.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleEScaledMomDev.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleESpher.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleEtEtaPhi.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleEtThetaPhi.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleMCCart.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleMCMomDev.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleMCPInvSpher.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleMCSpher.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleMomDev.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TFitParticleSpher.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TKinFitter.h";
+#pragma link C++ defined_in "HelperClasses/KinFitter/TSLToyGen.h";
