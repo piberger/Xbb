4c7ecf2a04d152a84de2d3ee1342978c5be3df9e
piberger <pberger@phys.ethz.ch>
2019-11-27 13:59:57 +0100

    take nominal DNN inputs from classifier checkpoint and create sys variations from replacement rules

--- python/sys_var.py
+++ python/sys_var.py
@@ -26,9 +26,29 @@ config = XbbConfigTools(config=XbbConfigReader.read(opts.tag))
 treeVarSet = config.get(opts.region, 'treeVarSet') 
 nominalVariables = config.get(treeVarSet, 'Nominal').split(' ')
 
+# test replacement rules
 replacementRulesList = XbbTools.getReplacementRulesList(config, opts.syst)
 for x in nominalVariables:
     sysVar = XbbTools.getSystematicsVariationTemplate(x, replacementRulesList) 
     sysVar = sysVar.replace('{syst}', opts.syst).replace('{UD}', opts.ud)
     print(sysVar)
 
+##replacementRulesList = XbbTools.getReplacementRulesList(config, opts.syst)
+##sysVars = []
+#for i,x in enumerate(nominalVariables):
+#    sysVar = XbbTools.getSystematicsVariationTemplate(x, replacementRulesList)
+#    sysVar = sysVar.replace('{syst}', opts.syst).replace('{UD}', opts.ud)
+#    print(sysVar)
+#    sysVars.append(sysVar)
+#    if varsFromConfig[i]!=sysVars[i]:
+#        print("\x1b[31mMISMATCH:")
+#        print(" config:   ", varsFromConfig[i])
+#        print(" generated:", sysVars[i], "\x1b[0m")
+
+# test XbbMvaInputsList class
+from myutils.XbbTools import XbbMvaInputsList
+inputsList = XbbMvaInputsList(nominalVariables, config=config)
+for i,x in enumerate(nominalVariables):
+    print("NOMINAL:", inputsList.get(i))
+    print("     UP:", inputsList.get(i, syst=opts.syst, UD='Up'))
+    print("   DOWN:", inputsList.get(i, syst=opts.syst, UD='Down'))
