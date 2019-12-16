b3c238e9d96352b96b293ec19f256456d275af98
piberger <pberger@phys.ethz.ch>
2019-12-16 17:12:19 +0100

    add new version of NLO weight, allow DNN branches to staert with DNN instead of BDT

--- python/myutils/LOtoNLOweight.py
+++ python/myutils/LOtoNLOweight.py
@@ -12,6 +12,9 @@ class LOtoNLOweight(AddCollectionsModule):
         super(LOtoNLOweight, self).__init__()
         self.branchName = branchName
         self.year = int(year)
+
+        self.addV3 = False
+        self.addV4 = True
             
         self.ZjetsToNuNuNLOweightNjet2017 = [
                 [0,0,1.228032,0.006932,-0.001342,0.000038],
@@ -73,6 +76,47 @@ class LOtoNLOweight(AddCollectionsModule):
                 [2,4,0.949781804,0.095093814,-0.001050688,0.000471687],
                 [2,5,0.709471838,0.160303941,-0.000694619,0.000742524]
             ]
+        self.ZjetsToNuNuNLOweight2017V4 = [
+            [
+                [1.22677725339,0.00311575545047,-0.00112963223927,1.29282259793e-05],
+                [1.29049031827,0.00461358046641,-0.00124756450272,1.94460346591e-05],
+                [1.3636793139,0.00380872777152,-0.00147972942279,1.57968703605e-05],
+                [1.38027222023,0.00423764008367,-0.0015447688251,1.76107315882e-05],
+                [1.40308681015,0.00484583833964,-0.00161204424483,2.02253690332e-05],
+                [1.43389363026,0.00571708611515,-0.00169512827031,2.39816294723e-05],
+                [1.46158117198,0.00694411641241,-0.00175257156669,2.9361789442e-05],
+                [1.49538841797,0.00864418260506,-0.00183752191762,3.68273172387e-05],
+                [1.50673458945,0.0111005152962,-0.0018229532664,4.78534425631e-05],
+                [1.5194558797,0.020088611979,-0.0018006013307,9.06177722205e-05],
+                [1.53032921477,0.0147428635733,-0.0014809810881,6.78293429486e-05]
+            ],
+            [
+                [1.16736127488,0.0121284431641,-0.00107476823455,4.98385067402e-05],
+                [1.24110889013,0.0125519094754,-0.00128726343266,5.15656976743e-05],
+                [1.24818352644,0.0139319992556,-0.0014163561105,5.72458255563e-05],
+                [1.28446754174,0.0157218011294,-0.00152302151278,6.50078558206e-05],
+                [1.3115449472,0.0176953960807,-0.0016501961541,7.29410162652e-05],
+                [1.32636417558,0.0217764785483,-0.00163611932605,9.12341112311e-05],
+                [1.34862919668,0.0384974157419,-0.00163294420101,0.000165453542905],
+                [1.45334247929,0.0494593511794,-0.00204285127658,0.000213486537985],
+                [1.493770589,0.0477271519291,-0.00203499950982,0.000207551408471],
+                [1.48657664816,0.0656064096975,-0.00187600001609,0.000290582178555],
+                [1.60806190872,0.0992119167923,-0.00255268993508,0.000456569756199]
+            ],
+            [
+                [0.97687993581,0.0165063765063,-0.000790248462335,6.69412274479e-05],
+                [1.0602744594,0.0178802047799,-0.00101398560128,7.23711369095e-05],
+                [1.17901054066,0.0216841821638,-0.00129990153026,8.74063731256e-05],
+                [1.2714700415,0.0257095173323,-0.0015415569768,0.000103859490534],
+                [1.29002891386,0.0311320734108,-0.00154839673828,0.000126614718642],
+                [1.42815664871,0.0399962299922,-0.00193721071673,0.000162485130853],
+                [1.40042261369,0.0530796123521,-0.00185747235945,0.000220476663638],
+                [1.44260749302,0.0730886547618,-0.00206568571207,0.000306464240409],
+                [1.17947501595,0.158249999217,-0.00162620925245,0.000693126503846],
+                [1.28595132842,0.120696407702,-0.00250134055258,0.000498755812061],
+                [0.7557556876,0.245657709045,-0.00054160616066,0.00107978539421]
+            ]
+        ]
 
     def customInit(self, initVars):
         self.sample = initVars['sample']
@@ -84,18 +128,30 @@ class LOtoNLOweight(AddCollectionsModule):
             self.addBranch(self.branchName + '_LHEVptV2_p0_Down')
             self.addBranch(self.branchName + '_LHEVptV2_p1_Up')
             self.addBranch(self.branchName + '_LHEVptV2_p1_Down')
-            self.addBranch(self.branchName + '_LHEVptV3')
-            for njet in [0,1,2,3,4,5]:
-                self.addBranch(self.branchName + '_LHEVptV3_njet%d_p0_Up'%njet)
-                self.addBranch(self.branchName + '_LHEVptV3_njet%d_p0_Down'%njet)
-                self.addBranch(self.branchName + '_LHEVptV3_njet%d_p1_Up'%njet)
-                self.addBranch(self.branchName + '_LHEVptV3_njet%d_p1_Down'%njet)
+
+            if self.addV3:
+                self.addBranch(self.branchName + '_LHEVptV3')
+                for njet in [0,1,2,3,4,5]:
+                    self.addBranch(self.branchName + '_LHEVptV3_njet%d_p0_Up'%njet)
+                    self.addBranch(self.branchName + '_LHEVptV3_njet%d_p0_Down'%njet)
+                    self.addBranch(self.branchName + '_LHEVptV3_njet%d_p1_Up'%njet)
+                    self.addBranch(self.branchName + '_LHEVptV3_njet%d_p1_Down'%njet)
+
+            if self.addV4:
+                self.addBranch(self.branchName + '_LHEVptV4')
+                for deltaEtaBin in [0,1,2,3,4,5,6,7,8,9,10]:
+                    self.addBranch(self.branchName + '_LHEVptV4_deta%d_p0_Up'%deltaEtaBin)
+                    self.addBranch(self.branchName + '_LHEVptV4_deta%d_p0_Down'%deltaEtaBin)
+                    self.addBranch(self.branchName + '_LHEVptV4_deta%d_p1_Up'%deltaEtaBin)
+                    self.addBranch(self.branchName + '_LHEVptV4_deta%d_p1_Down'%deltaEtaBin)
 
             self.addBranch(self.branchName + '_2016')
 
             self.sampleTree = initVars['sampleTree']
             self.nAddJetsFormula = "Sum$(GenJet_pt>30&&abs(GenJet_eta)<2.4)-2"
+            self.deltaEtaFormula = "min(int(abs(GenJet_eta[0]-GenJet_eta[1])/0.5),10)" 
             self.sampleTree.addFormula(self.nAddJetsFormula)
+            self.sampleTree.addFormula(self.deltaEtaFormula)
 
     def processEvent(self, tree):
         # if current entry has not been processed yet
@@ -108,10 +164,18 @@ class LOtoNLOweight(AddCollectionsModule):
             self._b(self.branchName + '_LHEVptV2_p1_Up')[0]   = 1.0
             self._b(self.branchName + '_LHEVptV2_p1_Down')[0] = 1.0
             self._b(self.branchName + '_2016')[0]             = 1.0
-            self._b(self.branchName + '_LHEVptV3')[0]         = 1.0
-            for njet in [0,1,2,3,4,5]:
-                for v in ['p0_Up', 'p0_Down', 'p1_Up', 'p1_Down']:
-                    self._b(self.branchName + '_LHEVptV3_njet%d_%s'%(njet, v))[0] = 1.0
+
+            if self.addV3:
+                self._b(self.branchName + '_LHEVptV3')[0]         = 1.0
+                for njet in [0,1,2,3,4,5]:
+                    for v in ['p0_Up', 'p0_Down', 'p1_Up', 'p1_Down']:
+                        self._b(self.branchName + '_LHEVptV3_njet%d_%s'%(njet, v))[0] = 1.0
+
+            if self.addV4: 
+                self._b(self.branchName + '_LHEVptV4')[0]         = 1.0
+                for n in [0,1,2,3,4,5,6,7,8,9,10]:
+                    for v in ['p0_Up', 'p0_Down', 'p1_Up', 'p1_Down']:
+                        self._b(self.branchName + '_LHEVptV4_deta%d_%s'%(n, v))[0] = 1.0
 
             if self.applies(tree):
                 etabb = abs(tree.Jet_eta[tree.hJidx[0]]-tree.Jet_eta[tree.hJidx[1]])
@@ -134,13 +198,24 @@ class LOtoNLOweight(AddCollectionsModule):
                         self._b(self.branchName + '_LHEVptV2_p1_Down')[0] = self.LOtoNLOWeightBjetSplitVpt2017V2(tree.LHE_Vpt, njets, self.sample.identifier, var1=-1.0)
 
                         # LOtoNLOWeightBjetSplitVptNjet2017V3
-                        naddjets = self.sampleTree.evaluate(self.nAddJetsFormula)
-                        self._b(self.branchName + '_LHEVptV3')[0]         = self.LOtoNLOWeightBjetSplitVptNjet2017V3(tree.LHE_Vpt, naddjets, njets, self.sample.identifier)
-                        for naddjet_var in [0,1,2,3,4,5]:
-                            self._b(self.branchName + '_LHEVptV3_njet%d_p0_Up'%(naddjet_var))[0] = self.LOtoNLOWeightBjetSplitVptNjet2017V3(tree.LHE_Vpt, naddjets, njets, self.sample.identifier, var0=1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
-                            self._b(self.branchName + '_LHEVptV3_njet%d_p0_Down'%(naddjet_var))[0] = self.LOtoNLOWeightBjetSplitVptNjet2017V3(tree.LHE_Vpt, naddjets, njets, self.sample.identifier, var0=-1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
-                            self._b(self.branchName + '_LHEVptV3_njet%d_p1_Up'%(naddjet_var))[0] = self.LOtoNLOWeightBjetSplitVptNjet2017V3(tree.LHE_Vpt, naddjets, njets, self.sample.identifier, var1=1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
-                            self._b(self.branchName + '_LHEVptV3_njet%d_p1_Down'%(naddjet_var))[0] = self.LOtoNLOWeightBjetSplitVptNjet2017V3(tree.LHE_Vpt, naddjets, njets, self.sample.identifier, var1=-1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
+                        if self.addV3:
+                            naddjets = self.sampleTree.evaluate(self.nAddJetsFormula)
+                            self._b(self.branchName + '_LHEVptV3')[0]         = self.LOtoNLOWeight2017V3(tree.LHE_Vpt, njets, naddjets, self.sample.identifier)
+                            for naddjet_var in [0,1,2,3,4,5]:
+                                self._b(self.branchName + '_LHEVptV3_njet%d_p0_Up'%(naddjet_var))[0] = self.LOtoNLOWeight2017V3(tree.LHE_Vpt, njets, naddjets, self.sample.identifier, var0=1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
+                                self._b(self.branchName + '_LHEVptV3_njet%d_p0_Down'%(naddjet_var))[0] = self.LOtoNLOWeight2017V3(tree.LHE_Vpt, njets, naddjets, self.sample.identifier, var0=-1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
+                                self._b(self.branchName + '_LHEVptV3_njet%d_p1_Up'%(naddjet_var))[0] = self.LOtoNLOWeight2017V3(tree.LHE_Vpt, njets, naddjets, self.sample.identifier, var1=1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
+                                self._b(self.branchName + '_LHEVptV3_njet%d_p1_Down'%(naddjet_var))[0] = self.LOtoNLOWeight2017V3(tree.LHE_Vpt, njets, naddjets, self.sample.identifier, var1=-1.0) if naddjets==naddjet_var else self._b(self.branchName + '_LHEVptV3')[0]
+                        
+                        # LOtoNLOWeightV4
+                        if self.addV4:
+                            deltaEtaBin = int(self.sampleTree.evaluate(self.deltaEtaFormula))
+                            self._b(self.branchName + '_LHEVptV4')[0]         = self.LOtoNLOWeight2017V4(tree.LHE_Vpt, deltaEtaBin,  njets, self.sample.identifier)
+                            for deltaEtaBin_var in [0,1,2,3,4,5,6,7,8,9,10]:
+                                self._b(self.branchName + '_LHEVptV4_deta%d_p0_Up'%(deltaEtaBin_var))[0]   = self.LOtoNLOWeight2017V4(tree.LHE_Vpt, deltaEtaBin, njets, self.sample.identifier, var0=1.0)  if deltaEtaBin==deltaEtaBin_var else self._b(self.branchName + '_LHEVptV4')[0]
+                                self._b(self.branchName + '_LHEVptV4_deta%d_p0_Down'%(deltaEtaBin_var))[0] = self.LOtoNLOWeight2017V4(tree.LHE_Vpt, deltaEtaBin, njets, self.sample.identifier, var0=-1.0) if deltaEtaBin==deltaEtaBin_var else self._b(self.branchName + '_LHEVptV4')[0]
+                                self._b(self.branchName + '_LHEVptV4_deta%d_p1_Up'%(deltaEtaBin_var))[0]   = self.LOtoNLOWeight2017V4(tree.LHE_Vpt, deltaEtaBin, njets, self.sample.identifier, var1=1.0)  if deltaEtaBin==deltaEtaBin_var else self._b(self.branchName + '_LHEVptV4')[0]
+                                self._b(self.branchName + '_LHEVptV4_deta%d_p1_Down'%(deltaEtaBin_var))[0] = self.LOtoNLOWeight2017V4(tree.LHE_Vpt, deltaEtaBin, njets, self.sample.identifier, var1=-1.0) if deltaEtaBin==deltaEtaBin_var else self._b(self.branchName + '_LHEVptV4')[0]
 
                     else:
                         self._b(self.branchName)[0] = 1.153 * self.LOtoNLOWeightBjetSplitEtabb(etabb, njets) 
@@ -264,7 +339,7 @@ class LOtoNLOweight(AddCollectionsModule):
         return SF
     
     
-    def LOtoNLOWeightBjetSplitVptNjet2017V3(self, vpt, nb, njets, sampleIdentifier, var0=0.0, var1=0.0):
+    def LOtoNLOWeight2017V3(self, vpt, nb, njets, sampleIdentifier, var0=0.0, var1=0.0):
         SF = 1.0
         vpt = max(min(vpt,500.0),50.0)
         if 'amc' not in sampleIdentifier:
@@ -286,3 +361,19 @@ class LOtoNLOweight(AddCollectionsModule):
                 SF = 0
         return SF
 
+    def LOtoNLOWeight2017V4(self, vpt, nb, deltaEtaBin, sampleIdentifier, var0=0.0, var1=0.0):
+        SF = 1.0
+        vpt = max(min(vpt,500.0),50.0)
+        if 'amc' not in sampleIdentifier:
+            nb = max(min(nb,2),0)
+            deltaEtaBin = max(min(deltaEtaBin,10),0)
+            coeffs = [1.0, 0.0, 0.0, 0.0]
+            norm = 1.0
+            if any([x in sampleIdentifier for x in ['ZJets', 'ZBJets']]):
+                coeffs = self.ZjetsToNuNuNLOweight2017V4[nb][deltaEtaBin]
+                # this factor is to keep total normalization fixed at LO * NNLO-k-factor 
+                norm = 0.86365
+            SF = norm * ((coeffs[0] + var0*coeffs[1]) + (coeffs[2] + var1*coeffs[3])*vpt)
+            if SF < 0.01:
+                SF = 0.01
+        return SF
