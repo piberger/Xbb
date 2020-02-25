a16391990ff235a3161f7c83309b71e615746599
Krunal Gedia (ETHZ) [krgedia] <kbgedia24@gmail.com>
2020-02-25 10:54:23 +0100

    add

---
+++ python/VHbb2018commonconfig/make_postfit_plots_multi.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "usage: $0 path/to/FitDiagnostics.root name"
+echo "this will create multi-DNN postfit plots!"
+
+./submit.py -T Zvv2017 -J postfitplot --set='Fit.FitDiagnosticsDump:='$1';Fit.FitType:=shapes_fit_s;Fit.regions:=<!Fit|regions_multi!>;Plot_general.mcUncertaintyLegend:="MC uncert.";plotDef:postfitCRDNN_BKG.log:=True' --local -F postfit_$2
+./submit.py -T Zvv2017 -J postfitplot --set='Fit.FitDiagnosticsDump:='$1';Fit.FitType:=shapes_prefit;Fit.regions:=<!Fit|regions_multi!>;Plot_general.mcUncertaintyLegend:="MC uncert.";plotDef:postfitCRDNN_BKG.log:=True' --local -F postfit_$2
+
+./submit.py -T Wlv2017 -J postfitplot --set='plotDef:postfitMultiDNNbackground.log:=True;Fit.regions:=<!Fit|regions_multi!>;Fit.FitDiagnosticsDump:='$1';Fit.FitType:=shapes_fit_s;Plot_general.mcUncertaintyLegend:="MC uncert."' --local -F postfit_$2
+./submit.py -T Wlv2017 -J postfitplot --set='plotDef:postfitMultiDNNbackground.log:=True;Fit.regions:=<!Fit|regions_multi!>;Fit.FitDiagnosticsDump:='$1';Fit.FitType:=shapes_prefit;Plot_general.mcUncertaintyLegend:="MC uncert."' --local -F postfit_$2
+
+./submit.py -T Zll2017 -J postfitplot --set='plotDef:postfitMultiDNNbackground.log:=True;Fit.regions:=<!Fit|regions_multi!>;Fit.FitDiagnosticsDump:='$1';Fit.FitType:=shapes_fit_s;Plot_general.mcUncertaintyLegend:="MC uncert."' --local -F postfit_$2
+./submit.py -T Zll2017 -J postfitplot --set='plotDef:postfitMultiDNNbackground.log:=True;Fit.regions:=<!Fit|regions_multi!>;Fit.FitDiagnosticsDump:='$1';Fit.FitType:=shapes_prefit;Plot_general.mcUncertaintyLegend:="MC uncert."' --local -F postfit_$2
+
+mkdir postfit_$2
+cp logs_Zll2017/postfit_$2/Plots/* postfit_$2/
+cp logs_Zvv2017/postfit_$2/Plots/* postfit_$2/
+cp logs_Wlv2017/postfit_$2/Plots/* postfit_$2/
