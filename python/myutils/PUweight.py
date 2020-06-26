a87535d6f9442164667fd742c18ebfbc9f59589c
piberger <pberger@phys.ethz.ch>
2020-06-26 15:00:59 +0200

    update PUweight module

--- python/myutils/PUweight.py
+++ python/myutils/PUweight.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 import ROOT
+from BranchTools import Collection
+from BranchTools import AddCollectionsModule
 
-class PUweight(object):
+class PUweight(AddCollectionsModule):
 
-    def __init__(self, fileNameData, fileNameMC, fileNameDataUp=None, fileNameDataDown=None, nano=False, puWeightName='puWeight'):
+    def __init__(self, fileNameData, fileNameMC, fileNameDataUp=None, fileNameDataDown=None, nano=True, puWeightName='puWeight'):
+        super(PUweight, self).__init__()
+        self.version = 2
         self.nano = nano
         self.puWeightName = puWeightName
         self.fileNameData = fileNameData
         self.fileNameDataUp = fileNameDataUp
         self.fileNameDataDown = fileNameDataDown
         self.fileNameMC = fileNameMC
-        self.branches = []
+        self.ignoreHistogramMismatch = True
 
     def customInit(self, initVars):
-        self.sample = initVars['sample']
+        self.sample     = initVars['sample']
         self.sampleTree = initVars['sampleTree']
+        self.config     = initVars['config']
         if self.sample.isMC():
             self.systematics = {
                     'Nominal': self.fileNameData, 
@@ -26,7 +31,7 @@ class PUweight(object):
             rootFileData = {k: ROOT.TFile.Open(v, "read") if v else None for k,v in self.systematics.iteritems()}
             if not rootFileData['Nominal']:
                 raise Exception("RootFileMissing")
-            histogramData = {k: v.Get('pileup') if v else None for k,v in rootFileData.iteritems()}
+            self.histogramData = {k: v.Get('pileup') if v else None for k,v in rootFileData.iteritems()}
 
             # if no file for MC is given, try to load MC pileup from 'autoPU' histogram (2017)
             if self.fileNameMC:
@@ -41,32 +46,35 @@ class PUweight(object):
                     raise Exception("NoPUforMC")
 
             # normalize histograms
-            for k,v in histogramData.iteritems():
+            for k,v in self.histogramData.iteritems():
                 if v:
                     v.Scale(1.0/v.Integral())
             histogramMC.Scale(1.0/histogramMC.Integral())
 
             # check histogram compatibility
-            for k,v in histogramData.iteritems():
+            for k,v in self.histogramData.iteritems():
                 if v:
-                    if v.GetNbinsX()!=histogramMC.GetNbinsX() or v.GetXaxis().GetXmin()!=histogramMC.GetXaxis().GetXmin() or v.GetXaxis().GetXmax()!=histogramMC.GetXaxis().GetXmax():
+                    if not self.ignoreHistogramMismatch and (v.GetNbinsX()!=histogramMC.GetNbinsX() or v.GetXaxis().GetXmin()!=histogramMC.GetXaxis().GetXmin() or v.GetXaxis().GetXmax()!=histogramMC.GetXaxis().GetXmax()):
                         print "\x1b[31mERROR: ", k, "histograms not compatible! (bins, xmin, xmax)\x1b[0m"
+                        print v.GetNbinsX(),histogramMC.GetNbinsX()
+                        print v.GetXaxis().GetXmin(),histogramMC.GetXaxis().GetXmin()
+                        print v.GetXaxis().GetXmax(),histogramMC.GetXaxis().GetXmax()
                         raise Exception("IncompatiblePUhistograms")
             
-            if not histogramData['Nominal']:
+            if not self.histogramData['Nominal']:
                 raise Exception("HistogramMissing")
 
             # determine PU range
-            minPU = int(histogramData['Nominal'].GetXaxis().GetBinLowEdge(1))
-            maxPU = int(histogramData['Nominal'].GetXaxis().GetBinLowEdge(histogramData['Nominal'].GetXaxis().GetNbins()))
+            minPU = int(self.histogramData['Nominal'].GetXaxis().GetBinLowEdge(1))
+            maxPU = int(self.histogramData['Nominal'].GetXaxis().GetBinLowEdge(self.histogramData['Nominal'].GetXaxis().GetNbins()))
 
             # compute table of PUweight vs. nTrueInt
             self.puWeightLUT = {}
-            for k,v in histogramData.iteritems():
+            for k,v in self.histogramData.iteritems():
                 self.puWeightLUT[k] = [1.0]*(maxPU+1)
             for pu in range(minPU, maxPU+1):
                 nMC = histogramMC.GetBinContent(histogramMC.FindBin(pu))
-                for k,v in histogramData.iteritems():
+                for k,v in self.histogramData.iteritems():
                     if v:
                         self.puWeightLUT[k][pu] = 1.0 * v.GetBinContent(v.FindBin(pu))/nMC  if nMC > 0 else 1.0
                 print "PU = ",pu," weight = ", self.puWeightLUT['Nominal'][pu]
@@ -74,24 +82,27 @@ class PUweight(object):
             self.puWeightLUTmax = maxPU 
 
             # add PU weight for all available systematic variations
-            for k,v in histogramData.iteritems():
+            for k,v in self.histogramData.iteritems():
                 if v:
-                    self.branches.append({'name': self.puWeightName + (('_'+k) if k != 'Nominal' else ''), 'formula': self.getPUweight, 'arguments': {'sys': k}})
+                    self.addBranch(self.puWeightName + (('_'+k) if k != 'Nominal' else ''))
             print('dictionary_pu',self.puWeightLUT)
 
-
-    def getBranches(self):
-        return self.branches
-
-    def getPUweight(self, tree, arguments=None):
+    def getPUweight(self, tree, syst=None):
         PUweight = 1
         pu = tree.Pileup_nTrueInt
         if pu < self.puWeightLUTmin and pu > 0:
-            PUweight = self.puWeightLUT[arguments['sys']][self.puWeightLUTmin]
+            PUweight = self.puWeightLUT[syst][self.puWeightLUTmin]
         elif pu > self.puWeightLUTmax:
-            PUweight = self.puWeightLUT[arguments['sys']][self.puWeightLUTmax]
+            PUweight = self.puWeightLUT[syst][self.puWeightLUTmax]
         else:
-            PUweight = self.puWeightLUT[arguments['sys']][int(pu)]
-        print('new_weight', PUweight)
+            PUweight = self.puWeightLUT[syst][int(pu)]
         return PUweight
+
+
+    def processEvent(self, tree):
+        if not self.hasBeenProcessed(tree) and not self.sample.isData():
+            self.markProcessed(tree)
+            for k,v in self.histogramData.iteritems():
+                if v:
+                    self._b(self.puWeightName + (('_'+k) if k != 'Nominal' else ''))[0] = self.getPUweight(tree, k)
 
