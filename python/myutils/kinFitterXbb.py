8238c22249bcf79e4a588ee8cbe86cf90e6d738e
Daniel Abercrombie <dabercro@mit.edu>
2020-07-20 10:00:41 -0400

    Delete TNamed objects from global hash table

--- python/myutils/kinFitterXbb.py
+++ python/myutils/kinFitterXbb.py
@@ -378,6 +378,11 @@ class kinFitterXbb(AddCollectionsModule):
                         self._b(self.bDict[syst]['llbbr_eta_fit'])[0]         = -99 
                         self._b(self.bDict[syst]['llbbr_mass_fit'])[0]        = -99
 
+                    del cons_MZ
+                    del cons_x
+                    del cons_y
+                    del fitter
+
                 else:
                     # no two resolved jets
                     self._b(self.bDict[syst]['H_pt_fit'])[0]          = -1 
