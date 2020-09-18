3452046af1bf6d088205ab8a1dede4b6db6ee251
Krunal Gedia (ETHZ) [krgedia] <kbgedia24@gmail.com>
2020-09-18 16:31:19 +0200

    added corrections to variations of all boosted systematics

--- python/myutils/mSD_scale_res_shift.py
+++ python/myutils/mSD_scale_res_shift.py
@@ -7,13 +7,13 @@ import array
 import os
 import math
 import numpy as np
+from XbbConfig import XbbConfigTools
 
 class mSD_scale_res_shift(AddCollectionsModule):
 
-    def __init__(self, year = None, backupPreviousCorrection=True):
+    def __init__(self, year = None):
         super(mSD_scale_res_shift, self).__init__()
         self.debug = 'XBBDEBUG' in os.environ
-        self.backupPreviousCorrection = backupPreviousCorrection
 
         self.year = year if type(year) == str else str(year)
         self.scale_params = {
@@ -33,50 +33,67 @@ class mSD_scale_res_shift(AddCollectionsModule):
         self.sampleTree = initVars['sampleTree']
         self.isData = initVars['sample'].isData()
         self.sample = initVars['sample']
+        self.config      = initVars['config']
+        self.xbbConfig   = XbbConfigTools(self.config)
+        self.systematics = self.xbbConfig.getJECuncertainties()
+        self.systematicsBoosted = [x for x in self.systematics if 'jerReg' not in x] + ['jms', 'jmr']
+        self.maxnFatJet   = 256
 
         if self.sample.isMC():
-            self.maxnFatJet   = 256
+
             self.FatJet_Msoftdrop         = array.array('f', [0.0]*self.maxnFatJet)
             self.FatJet_msoftdrop_nom     = array.array('f', [0.0]*self.maxnFatJet)
-            self.FatJet_msoftdrop_jmrUp   = array.array('f', [0.0]*self.maxnFatJet)
-            self.FatJet_msoftdrop_jmrDown = array.array('f', [0.0]*self.maxnFatJet)
-            self.FatJet_msoftdrop_jmsUp   = array.array('f', [0.0]*self.maxnFatJet)
-            self.FatJet_msoftdrop_jmsDown = array.array('f', [0.0]*self.maxnFatJet)
+
             self.sampleTree.tree.SetBranchAddress("FatJet_Msoftdrop", self.FatJet_Msoftdrop)
             self.sampleTree.tree.SetBranchAddress("FatJet_msoftdrop_nom", self.FatJet_msoftdrop_nom)
-            self.sampleTree.tree.SetBranchAddress("FatJet_msoftdrop_jmrUp", self.FatJet_msoftdrop_jmrUp)
-            self.sampleTree.tree.SetBranchAddress("FatJet_msoftdrop_jmrDown", self.FatJet_msoftdrop_jmrDown)
-            self.sampleTree.tree.SetBranchAddress("FatJet_msoftdrop_jmsUp", self.FatJet_msoftdrop_jmsUp)
-            self.sampleTree.tree.SetBranchAddress("FatJet_msoftdrop_jmsDown", self.FatJet_msoftdrop_jmsDown)
 
-            if self.backupPreviousCorrection:
-                self.addVectorBranch("FatJet_MsoftdropOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_MsoftdropOld[nJet]/F")
-                self.addVectorBranch("FatJet_msoftdrop_nomOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_msoftdrop_nomOld[nJet]/F")
-                self.addVectorBranch("FatJet_msoftdrop_jmrUpOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_msoftdrop_jmrUpOld[nJet]/F")
-                self.addVectorBranch("FatJet_msoftdrop_jmrDownOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_msoftdrop_jmrDownOld[nJet]/F")
-                self.addVectorBranch("FatJet_msoftdrop_jmsUpOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_msoftdrop_jmsUpOld[nJet]/F")
-                self.addVectorBranch("FatJet_msoftdrop_jmsDownOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_msoftdrop_jmsDownOld[nJet]/F")
+            #create backup branches
+            self.addVectorBranch("FatJet_MsoftdropOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_MsoftdropOld[nFatJet]/F")
+            self.addVectorBranch("FatJet_msoftdrop_nomOld",     default=0.0, branchType='f', length=self.maxnFatJet, leaflist="FatJet_msoftdrop_nomOld[nFatJet]/F")
+
+            self.FatJet_msoftdrop_syst  = {}
+            for syst in self.systematicsBoosted:
+                self.FatJet_msoftdrop_syst[syst] = {}
+                for Q in self._variations(syst):
+
+                    self.FatJet_msoftdrop_syst[syst][Q]  = array.array('f', [0.0]*self.maxnFatJet)
+                    self.sampleTree.tree.SetBranchAddress("FatJet_msoftdrop_"+syst+Q, self.FatJet_msoftdrop_syst[syst][Q])
+                    
+                    #backup branches
+                    self.addVectorBranch('FatJet_msoftdrop_'+syst+Q+'Old', default=0.0, branchType='f', length=self.maxnFatJet,leaflist='FatJet_msoftdrop_'+syst+Q+'Old[nFatJet]/F')
 
     def processEvent(self, tree):
         if not self.hasBeenProcessed(tree) and self.sample.isMC():
             self.markProcessed(tree)
             
-            nJet = tree.nFatJet
+            nFatJet = tree.nFatJet
 
-            if self.backupPreviousCorrection:
-                for i in range(nJet):
+            if self.sample.isMC():
+                for i in range(nFatJet):
+
+                    #fill backup branches
                     self._b("FatJet_MsoftdropOld")[i]     = self.FatJet_Msoftdrop[i]
                     self._b("FatJet_msoftdrop_nomOld")[i]   = self.FatJet_msoftdrop_nom[i]
-                    self._b("FatJet_msoftdrop_jmrUpOld")[i] = self.FatJet_msoftdrop_jmrUp[i]
-                    self._b("FatJet_msoftdrop_jmrDownOld")[i] = self.FatJet_msoftdrop_jmrDown[i]
-                    self._b("FatJet_msoftdrop_jmsUpOld")[i] = self.FatJet_msoftdrop_jmsUp[i]
-                    self._b("FatJet_msoftdrop_jmsDownOld")[i] = self.FatJet_msoftdrop_jmsDown[i]
 
-            for i in range(nJet):
+                    #overwrite branches
+                    self.FatJet_Msoftdrop[i]     = self._b('FatJet_MsoftdropOld')[i]*self.scale
+                    self.FatJet_msoftdrop_nom[i] = self._b("FatJet_msoftdrop_nomOld")[i]*self.scale
 
-                    self.FatJet_Msoftdrop[i]         = self.FatJet_Msoftdrop[i]*self.scale
-                    self.FatJet_msoftdrop_nom[i]     = self.FatJet_msoftdrop_nom[i]*self.scale
-                    self.FatJet_msoftdrop_jmrUp[i]   = self.FatJet_msoftdrop_jmrUp[i]*self.res
-                    self.FatJet_msoftdrop_jmrDown[i] = self.FatJet_msoftdrop_jmrDown[i]*self.res
-                    self.FatJet_msoftdrop_jmsUp[i]   = self.FatJet_msoftdrop_nom[i]*self.scale_up 
-                    self.FatJet_msoftdrop_jmsDown[i] = self.FatJet_msoftdrop_nom[i]*self.scale_down
+                    for syst in self.systematicsBoosted:
+                        for Q in self._variations(syst):
+                            
+                            # fill backup branches
+                            self._b("FatJet_msoftdrop_"+syst+Q+'Old')[i] = self.FatJet_msoftdrop_syst[syst][Q][i]
+
+                            # overwrite branches
+                            if syst=='jms':
+                                if Q=='Up':
+                                    self.FatJet_msoftdrop_syst[syst][Q][i] = self._b("FatJet_msoftdrop_nomOld")[i]*self.scale_up
+                                else:
+                                    self.FatJet_msoftdrop_syst[syst][Q][i] = self._b("FatJet_msoftdrop_nomOld")[i]*self.scale_down
+                            elif syst=='jmr':
+                                self.FatJet_msoftdrop_syst[syst][Q][i] = self._b("FatJet_msoftdrop_"+syst+Q+'Old')[i]*self.res
+
+                            else:    
+                                self.FatJet_msoftdrop_syst[syst][Q][i]  = self._b("FatJet_msoftdrop_"+syst+Q+'Old')[i]*self.scale
+
