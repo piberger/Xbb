58e7bd134dd7aa3c5f97a1ed35d6b9a5f6ea6385
Krunal Gedia (ETHZ) [krgedia] <kbgedia24@gmail.com>
2020-10-05 16:52:00 +0200

    remove unsmearing code also fix JEC and apply scale up down variations when no gen jet is matched
    Co-authored-by: piberger <pberger@phys.ethz.ch>

--- python/myutils/JetSmearer.py
+++ python/myutils/JetSmearer.py
@@ -7,6 +7,10 @@ import array
 import os
 import math
 import numpy as np
+from XbbConfig import XbbConfigReader
+from sampleTree import SampleTree 
+from sample_parser import ParseInfo
+import BetterConfigParser
 
 # applies the smearing to MC jet resolution and modifies the Jet_PtReg* branches of the tree
 class JetSmearer(AddCollectionsModule):
@@ -19,25 +23,9 @@ class JetSmearer(AddCollectionsModule):
         self.quickloadWarningShown = False
 
         self.year = year if type(year) == str else str(year)
-        # sicne again wrong smearing was used in post-processor, undo that one first
-        self.unsmear_params = {
-                 '2016': [1.0, 0.0, 0.0, 0.0],
-                 '2017': [1.003, 0.021, 0.031, 0.053],
-                 '2018': [0.987, 0.020, 0.038, 0.054],
-                 }
-        # then afterwards apply the correct smearing
-        self.smear_params = {
-                 '2016': [1.013, 0.014, 0.029, 0.047],
-                 '2017': [1.017, 0.021, 0.058, 0.066],
-                 '2018': [0.985, 0.019, 0.080, 0.073],
-                 }
-        self.applyToNtupleVersions = ['V13']
-        if self.year not in self.smear_params:
-            print("ERROR: smearing for year", self.year, " not available!")
-            raise Exception("SmearingError")
+
 
         self.scale, self.scale_err, self.smear, self.smear_err = self.smear_params[self.year]
-        self.undo_scale, self.undo_scale_err, self.undo_smear, self.undo_smear_err = self.unsmear_params[self.year]
 
     def customInit(self, initVars):
         self.sampleTree = initVars['sampleTree']
@@ -45,23 +33,37 @@ class JetSmearer(AddCollectionsModule):
         self.sample = initVars['sample']
         self.config = initVars['config']
 
-        if self.config.get('General','nTupleVersion') in self.applyToNtupleVersions:
-            self.active = True
-            print("INFO: regressed jet resolution correction is active!")
+        #smearing params
+        if self.config.get('General','nTupleVersion') in ['V11','V12']:
+            self.smear_params = {
+                     '2016': [1.0, 0.0, 0.0, 0.0],
+                     '2017': [1.0029846959, 0.0212893588055, 0.030684, 0.052497],
+                     '2018': [0.98667384694, 0.0197153848807, 0.038481, 0.053924],
+                     }
+        elif self.config.get('General','nTupleVersion') == 'V13':
+            self.smear_params = {
+                     '2016': [1.013, 0.014, 0.029, 0.047],
+                     '2017': [1.017, 0.021, 0.058, 0.066],
+                     '2018': [0.985, 0.019, 0.080, 0.073],
+                     }
         else:
-            self.active = False
-            print("INFO: regressed jet resolution correction is NOT active!")
+            print("ERROR: jet smearing not defined for", self.config.get('General','nTupleVersion'),"!")
+            raise Exception("SmearingError")
 
-        if self.sample.isMC() and self.active:
-            # resolutions used in post-processor smearing
-            #self.unsmearResNom  = 1.1
-            #self.unsmearResUp   = 1.2
-            #self.unsmearResDown = 1.0
+        if self.year not in self.smear_params:
+            print("ERROR: smearing for year", self.year, " not available!")
+            raise Exception("SmearingError")
 
-            self.maxNjet   = 256
-            self.PtReg     = array.array('f', [0.0]*self.maxNjet)
-            self.PtRegUp   = array.array('f', [0.0]*self.maxNjet)
-            self.PtRegDown = array.array('f', [0.0]*self.maxNjet)
+        if self.sample.isMC():
+
+            self.maxNjet      = 256
+            self.bRegCorr     = array.array('f', [0.0]*self.maxNjet)
+            self.Pt           = array.array('f', [0.0]*self.maxNjet)
+            self.PtReg        = array.array('f', [0.0]*self.maxNjet)
+            self.PtRegUp      = array.array('f', [0.0]*self.maxNjet)
+            self.PtRegDown    = array.array('f', [0.0]*self.maxNjet)
+            self.sampleTree.tree.SetBranchAddress("Jet_bRegCorr", self.bRegCorr)
+            self.sampleTree.tree.SetBranchAddress("Jet_Pt", self.Pt)
             self.sampleTree.tree.SetBranchAddress("Jet_PtReg", self.PtReg)
             self.sampleTree.tree.SetBranchAddress("Jet_PtRegUp", self.PtRegUp)
             self.sampleTree.tree.SetBranchAddress("Jet_PtRegDown", self.PtRegDown)
@@ -74,82 +76,39 @@ class JetSmearer(AddCollectionsModule):
     def processEvent(self, tree):
         if self.active and not self.hasBeenProcessed(tree) and self.sample.isMC():
             self.markProcessed(tree)
-            
+
             nJet = tree.nJet
 
-            # backup the Jet_PtReg branches with the old smearing
+            # backup the Jet_PtReg branches as they are in post-processed ntuples
             if self.backupPreviousCorrection:
                 for i in range(nJet):
                     self._b("Jet_PtRegOld")[i]     = self.PtReg[i]
                     self._b("Jet_PtRegOldUp")[i]   = self.PtRegUp[i]
                     self._b("Jet_PtRegOldDown")[i] = self.PtRegDown[i]
 
-            # original V5 post-procesor smearing which is undone:
-            # if isMC:
-            #     # until we have final post-regression smearing factors we assume a flat 10%
-            #     if sysVar==0: # nominal
-            #        resSmear = 1.1
-            #    elif sysVar==1: # up
-            #        resSmear = 1.2
-            #    elif sysVar==-1: # down
-            #        resSmear = 1.0
-            #    smearedPt = jet.pt*jet.bRegCorr 
-            #    if jet.genJetIdx >=0 and  jet.genJetIdx < len(self.genJetsWithNeutrinos) :
-            #        genJet=self.genJetsWithNeutrinos[jet.genJetIdx]
-            #        dPt = smearedPt - genJet.Pt()
-            #        smearedPt=genJet.Pt()+resSmear*dPt
-            #    return smearedPt
-
-            # original V13 smearing which is undone
-            #if gen_pt:
-            #gen_diff = regressed - gen_pt
-            #nominal = max(0.0, (gen_pt + gen_diff * (1.0 + self.smear_params.smear)) * self.smear_params.scale)
-            #band = math.sqrt(pow(nominal/self.smear_params.scale * self.smear_params.scale_err, 2) +
-            #                 pow(gen_diff * self.smear_params.scale * self.smear_params.smear_err, 2))
-            #down, up = \
-            #    (max(nominal - band, no_smear), nominal + band) \
-            #    if regressed > gen_pt else \
-            #    (min(nominal + band, no_smear), nominal - band)
-            #return SmearResult(down, nominal, up)
-
-      # undo old smearing
-            if self.unsmearPreviousCorrection:
-                for i in range(nJet):
-                    genJetIdx = tree.Jet_genJetIdx[i]
-                    if genJetIdx > -1 and genJetIdx < len(tree.GenJetWithNeutrinos_pt):
-                        genJetPt = tree.GenJetWithNeutrinos_pt[genJetIdx]
-
-                        #self.PtReg[i]     = genJetPt + (self.PtReg[i] - genJetPt)/self.unsmearResNom
-                        #self.PtRegUp[i]   = genJetPt + (self.PtRegUp[i] - genJetPt)/self.unsmearResUp 
-                        #self.PtRegDown[i] = genJetPt + (self.PtRegDown[i] - genJetPt)/self.unsmearResDown
-                        
-                        self.PtReg[i]     = genJetPt + (self.PtReg[i]/self.undo_scale - genJetPt)/(1.0+self.undo_smear)
-
-                        # new version of smearing does not allow us to go back for up/down variations 
-                        self.PtRegUp[i]   = -1
-                        self.PtRegDown[i] = -1
-
-                    # after undoing the smearing, check if up/down variations are the same
-                    # assert (max(abs(self.PtReg[i]-self.PtRegUp[i]),abs(self.PtRegUp[i]-self.PtRegDown[i])) < 0.001 or self.PtReg[i] < 0)
-
-            # apply new smearing
+            # apply smearing
             for i in range(nJet):
                 genJetIdx = tree.Jet_genJetIdx[i]
                 if genJetIdx > -1 and genJetIdx < len(tree.GenJetWithNeutrinos_pt):
                     gen_pt    = tree.GenJetWithNeutrinos_pt[genJetIdx]
 
                     # reference: https://github.com/dabercro/hbb/blob/b86589128a6839a12efaf041f579fe88c1d1be38/nanoslimmer/applysmearing/applysmearing.py
-                    regressed = self.PtReg[i]
+                    regressed = self.Pt[i]*self.bRegCorr[i]
                     no_smear  = regressed * self.scale
                     gen_diff  = regressed - gen_pt
                     nominal   = max(0.0, (gen_pt + gen_diff * (1.0 + self.smear)) * self.scale)
                     band      = math.sqrt(pow(nominal/self.scale * self.scale_err, 2) + pow(gen_diff * self.scale * self.smear_err, 2))
 
                     down, up  = (max(nominal - band, no_smear), nominal + band) if regressed > gen_pt else (min(nominal + band, no_smear), nominal - band)  
+                else:
+                    regressed = self.Pt[i] * self.bRegCorr[i]
+                    nominal   = regressed * self.scale
+                    up        = regressed * (self.scale + self.scale_err)
+                    down      = regressed * (self.scale - self.scale_err)
 
-                    self.PtReg[i]     = nominal
-                    self.PtRegUp[i]   = up
-                    self.PtRegDown[i] = down
+                self.PtReg[i]     = nominal
+                self.PtRegUp[i]   = up
+                self.PtRegDown[i] = down
 
             # formulas by default reload the branch content when evaluating the first instance of the object!
             # SetQuickLoad(1) turns off this behavior
@@ -164,3 +123,28 @@ class JetSmearer(AddCollectionsModule):
                 #    raise Exception("NotImplemented")
 
 
+if __name__ == '__main__':
+
+    config = XbbConfigReader.read('Zvv2018')
+
+    info = ParseInfo(config=config)
+    
+    sample = [x for x in info if x.identifier == 'ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8'][0]
+    # read sample
+    sampleTree = SampleTree(['/store/group/phys_higgs/hbb/ntuples/VHbbPostNano/2018/V13/ZH_HToBB_ZToNuNu_M125_13TeV_powheg_pythia8/RunIIAutumn18NanoAODv7-Nano02A85/200519_095652/0000/tree_1.root'], treeName='Events', xrootdRedirector="root://eoscms.cern.ch/")
+    # initialize module
+    w = JetSmearer("2018")
+    w.customInit({'sampleTree': sampleTree, 'sample': sample})
+    n=0
+    for event in sampleTree:
+        w.processEvent(event)
+        n=n+1
+        if n==3: break
+
+    #sampleTree.addOutputBranches(w.getBranches()) 
+
+    # output files
+    #sampleTree.addOutputTree('/scratch/krgedia/test.root', cut='MET_Pt>0', branches='*')
+
+    # loop over all events!
+    #sampleTree.process()
